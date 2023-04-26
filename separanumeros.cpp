cf7a41d7037bf30fe8b091c0d67325944663f021
Walber S Mota <mota.csharp@gmail.com>
2023-04-26 02:26:55 -0200

    Programa Separa Digitos de um int

--- separanumeros.cpp
+++ separanumeros.cpp
@@ -1,25 +1,31 @@
-#include <iostream>
-using namespace std;
+/*
+Autor: WalberMota
+Data da Criação: 26/04/2023
+Objetivos: separar os numeros de um int com cinco digitos
+Linguagem C++
+ */
 
-int main() 
+#include <iostream>
+
+int main()
 {
     int sobra;
     int dig;
-    int modulo=10000;
+    int modulo = 10000;
     int num = 42339;
-    cout<< "insira um numero de cinco dígitos"<<endl;
-    cin >>num;
+    std::cout << "insira um numero de cinco dígitos" << std::endl;
+    std::cin >> num;
 separa:
-        dig = num % modulo; // módulo (resto da divisão) por 10
-        sobra = int(num / modulo); // divisão inteira por 10
-        modulo/=10;
-        num=dig;
-        cout <<sobra<<"   ";
-        if (dig==0)
-        {
-            cout << endl;
-            return 0;
-        }
-            
+    dig = num % modulo;        // módulo (resto da divisão) por 10
+    sobra = int(num / modulo); // divisão inteira por 10
+    modulo /= 10;
+    num = dig;
+    std::cout << sobra << "   ";
+    if (dig == 0)
+    {
+        std::cout << std::endl;
+        return 0;
+    }
+
     goto separa;
 }
