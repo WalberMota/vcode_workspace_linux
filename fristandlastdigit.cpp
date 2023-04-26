811abb8651780602aa8beb9e0e00b2a4f2a33a39
Walber S Mota <mota.csharp@gmail.com>
2023-04-26 21:26:30 -0200

    le o soma primeiro e ultimo digito de um int

---
+++ fristandlastdigit.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<math.h>
+
+int main()
+{
+	long int num,sum=0,firstDigit,lastDigit;
+	std::cout <<"Digite qualquer número: "<<"\n";
+	std::cin >> num;
+	
+	lastDigit=num%10;
+	firstDigit=num;
+	while (num >= 10)
+	{
+	num/=10;
+	}
+	firstDigit=num;
+	
+	sum=firstDigit+lastDigit;
+	
+	std::cout << "Primeiro Dígito: "<<firstDigit<<"\n";
+	std::cout << "Último Dígito: " <<lastDigit<<"\n";
+	std::cout << "Soma dos primeiro com o ultimo dígito: "<<sum<<"\n";
+	
+	return 0;
+}
