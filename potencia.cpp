06bb1c85e35256b36b172eb2c246199ac8acc886
Walber S Mota <mota.csharp@gmail.com>
2023-04-27 17:52:31 -0200

    Potencia.cpp
    Calculo de potencia de x^y

---
+++ potencia.cpp
@@ -0,0 +1,30 @@
+/*
+Autor: Walber Mota
+Data da Criação: 27/04/2023
+Objetivos: calcula x^y
+*/
+
+#include <iostream>
+
+int pot(int base, int expoente)
+{
+    int contador, resultado = 1;
+    for (contador = 0; contador < expoente; contador++)
+    {
+        resultado = resultado * base;
+    }
+    return resultado;
+}
+
+int main()
+{
+    int base,expo;
+    std::cout <<"Calculo da potencia de um dado numero inteiro\n\n";
+    std::cout <<"Digite a base ";
+    std::cin>>base;
+    std::cout <<"Digite o expoente ";
+    std::cin>>expo;
+    std::cout << "\nO resultado é "<<pot(base, expo) << "\n";
+
+    return 0;
+}
