0448fc5e73bc0a37bd97d905b62cbd43c71b28f5
Walber S Mota <mota.csharp@gmail.com>
2023-05-13 15:15:27 -0400

    Create ExemploFuncao.cpp
    Verificar se um numero é par ou impar com uso de função.

---
+++ ExemploFuncao.cpp
@@ -0,0 +1,29 @@
+/*
+Autor: Walber Mota
+Data da Criação: 13/05/2023
+Objetivos: uso de função (verifica se um número é par ou impar)
+Linguagem: c/c++
+*/
+
+#include <iostream>
+
+bool seForPar(int tipo)
+{
+    if (tipo % 2 == 0)
+        return true;
+        return false;
+}
+
+int main()
+{
+    int num;
+    std::cout << "Digite um numero inteiro : ";
+    std::cin >> num;
+    if (seForPar(num))
+        std::cout << "\nO número " << num << " é par\n";
+    else
+        std::cout << "\nO número " << num << " é impar\n";
+
+    std::cout<<"\neow...";
+    return 0;
+}
