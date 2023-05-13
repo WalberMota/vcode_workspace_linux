9e4a45b4b9c7268ed7262c08fea8941691801229
Walber S Mota <mota.csharp@gmail.com>
2023-05-13 16:33:01 -0400

    Create calculadora.cpp
    Uso de funções em calculadora primitiva

---
+++ calculadora.cpp
@@ -0,0 +1,45 @@
+/*
+Autor: Walber Mota
+Data da Criação: 13/05/2023
+Objetivos: calculadora baśica comuso de funções
+Linguagem: C/C++
+*/
+
+#include <iostream>
+
+// funções básicas
+float somar(float num1, float num2)
+{
+    return num1 + num2;
+}
+float subtrair(float num1, float num2)
+{
+    return num1 - num2;
+}
+float multiplicar(float num1, float num2)
+{
+    return num1 * num2;
+}
+float dividir(float num1, float num2)
+{
+    return num1 / num2;
+}
+
+// enter point do programa
+int main(int argc, char const *argv[])
+{
+    float n1, n2;
+    std::cout << "*****Calculadora automática ente dois numeros****\n";
+    std::cout << "digite o primeiro numero ";
+    std::cin >> n1;
+    std::cout << "digite o segundo numero  ";
+    std::cin >> n2;
+
+    std::cout << "Operações básica entre " << n1 << " e " << n2 << "\n\n";
+    std::cout << n1 << " + " << n2 << " = " << somar(n1, n2) << "\n";
+    std::cout << n1 << " - " << n2 << " = " << subtrair(n1, n2) << "\n";
+    std::cout << n1 << " X " << n2 << " = " << multiplicar(n1, n2) << "\n";
+    std::cout << n1 << " / " << n2 << " = " << dividir(n1, n2) << "\n";
+
+    return 0;
+}
