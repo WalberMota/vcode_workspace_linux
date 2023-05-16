5e6ca4655d9bdbba1fa7878f949821253491560f
Walber S Mota <mota.csharp@gmail.com>
2023-05-16 17:16:56 -0400

    Revert "MathSystem"
    This reverts commit 1a8357eec2c391da0e4e5c3ce4110b641273ea8f.

---
+++ ProjMath/LibMath.h
@@ -0,0 +1,10 @@
+//
+
+
+int Somar(int num1,int num2);
+int Subtrair(int num1,int num2);
+int Multiplicar(int num1,int num2);
+float Dividir(int num1,int num2);
+float Potencia(int num1,int num2);
+float RaizQuad(int num);
+
