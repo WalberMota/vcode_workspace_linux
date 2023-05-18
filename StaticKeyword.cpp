d8b1f15c7b8426ffbb42f68078a8dfb828f761f0
Walber S Mota <mota.csharp@gmail.com>
2023-05-18 13:53:59 -0400

    Estudo com static keyword (grando variaveis perenes)

---
+++ StaticKeyword.cpp
@@ -0,0 +1,29 @@
+/*
+Autor: WalberMota
+Data da Criação: 18/05/2023
+Objetivos: uso da palavra chave "static" 
+Linguagem C++
+ */
+
+#include <iostream>
+
+int ContadorChamadas()
+{
+	//Variavel estática: Significa que após a saída da função o valor desta variável não será perdido.
+	//static indica que este valor seja mantido mesmo após a função ser encerrada
+	int NumChamadasDaFuncao = 0;
+	NumChamadasDaFuncao++;
+    std::cout<<NumChamadasDaFuncao<<"\t";
+	return NumChamadasDaFuncao;
+}
+
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << ContadorChamadas() << std::endl;
+        
+	}
+
+	return 0;
+}
