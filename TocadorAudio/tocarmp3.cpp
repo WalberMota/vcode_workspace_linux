fbf582679c7bb473dde799130ace9a7d386b346e
Walber S Mota <mota.csharp@gmail.com>
2023-05-19 20:45:44 -0400

    Update tocarmp3.cpp

--- TocadorAudio/tocarmp3.cpp
+++ TocadorAudio/tocarmp3.cpp
@@ -1,7 +1,7 @@
 /*
 Autor: Walber Mota
 Data da Criação: 19/05/2023
-Objetivos: Prog. básico para reprodução de arquivo *.wav
+Objetivos: Prog. básico para reprodução de arquivo *.mp3
 Linguagem: C/C++
 OBS.: necessário instalar ffmpeg: sudo apt install ffmpeg
 */
@@ -10,7 +10,7 @@ OBS.: necessário instalar ffmpeg: sudo apt install ffmpeg
 
 int main()
 {
-    system("ffplay -nodisp -hide_banner -v quiet -autoexit -i 2310.wav");
+    system("ffplay -nodisp -hide_banner -v quiet -autoexit -i 2310.mp3");
     
     return 0;
 }
