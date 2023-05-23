21ec58d4d054b2f51e0bc92d8d9595c6de835934
Walber S Mota <mota.csharp@gmail.com>
2023-05-23 19:09:51 -0400

    Create operacoesponteiros2.cpp

---
+++ operacoesponteiros2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+	int Numero{ 4096 };
+	int *ptr;
+	int *ptr2;
+	ptr = &Numero;
+	ptr2 = ptr;
+    
+    std::cout << "\nValor da variável Numero: " << Numero << "\n";	
+    std::cout << "\nEndereço contido dentro de ptr=&Numero: " << ptr << "\n";
+	std::cout << "\nEndereço contido dentro de ptr2=ptr: " << ptr2 << "\n";
+	std::cout << "\nEndereço de ptr na memória RAM: " << &ptr << "\n";
+	
+	//Na aula esqueci de colocar & na frente de ptr2, portanto é &ptr2
+	//pois você deseja que seja buscado o endereço de memória RAM que ptr2 esta alocado e carregado
+
+	std::cout << "\nEndereço de ptr2 na memória RAM: " << &ptr2 << "\n";
+	std::cout << "\nValor de Numero agora: " << Numero << "\n";
+		*ptr2 = *ptr + 10;
+	std::cout << "\nValor de Numero depois de *ptr2 =*ptr + 10: " << Numero << "\n";
+
+	return 0;
+}
