702ce08271889b1714c4438cdf80e5720b5aeba1
Walber S Mota <mota.csharp@gmail.com>
2023-05-24 14:30:54 -0400

    Update de Exercicioponteiros.cpp

--- Exerciciosponteiro.cpp
+++ Exerciciosponteiro.cpp
@@ -1,3 +1,20 @@
+/* Autor:Walber Mota
+ Linguagem: C++
+ Criado em 24/05/2023
+ Objetivo: Exercicios propostos pelo mestre Marcos Pacheco utilizando ponteiros.
+
+Crie uma variável de nome Numero do Tipo inteiro e valor inicial igual a 234;
+Crie um ponteiro de nome Ponteiro capaz de apontar para um variável do tipo inteiro.
+Troque o valor de Numero para 1456 usando Ponteiro
+Coloque o valor de Numero na tela usando Ponteiro
+Mostre o endereço da memória RAM em que estão carregados, alocados as variáveis Ponteiro e Numero
+Crie um outro ponteiro de nome OutroPtr
+Faça OutroPtr apontar para a mesma variável que ptr aponta. 
+Usando Ponteiro some 200 ao valor contido em Numero
+Usando OutroPtr coloque na tela o valor de Numero
+
+*/
+
 #include <iostream>
 
 int main(int argc, char const *argv[])
