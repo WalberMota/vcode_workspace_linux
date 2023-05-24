05b951eaa27f876797dfb4368ef1da206ac2e03b
Walber S Mota <mota.csharp@gmail.com>
2023-05-23 20:16:22 -0400

    Criado ponteirogenerico-void.cpp

---
+++ ponteirogenerico-void.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    int Numero = 4890;
+    char Letra = 'E';
+
+    void *ptrG; // criando um ponterio genérico (usando a atribuição void)
+
+    ptrG = &Letra; // passa o endereço na memoria RAM da variável Letra dizendo ao compilador o tipo
+
+    std::cout << "Valor de da variável Letra via ponteiro generico criado " << *(char *)ptrG << "\n";
+
+    ptrG = &Numero; // passa o endereço na memória RAM da variável Numero dizendo ao compilador o tipo
+
+    std::cout << "Valor da variável Numero via ponteiro genérico criado " << *(int *)ptrG << "\n";
+
+    return 0;
+}
