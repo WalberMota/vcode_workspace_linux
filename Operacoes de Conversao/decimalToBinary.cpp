852782db5c4e524fb3608c32b5d85185b4059aba
Walber S Mota <mota.csharp@gmail.com>
2023-06-04 20:07:42 -0230

    Update decimalToBinary.cpp
    Update decimalToBinary.cpp

--- Operacoes de Conversao/decimalToBinary.cpp
+++ Operacoes de Conversao/decimalToBinary.cpp
@@ -2,6 +2,9 @@
 Autor:Walber Mota
 Linguagem: C++
 Criado em 24/04/2023
+Version 01.00 
+locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM)
+i5-5200U CPU @ 2.20GHz (306D4)
 Conversor de decimal em binario,hexadecimal e octal
 */
 
