34f0de14c9f0574ce8a085313c8852e6013deb95
Walber S Mota <mota.csharp@gmail.com>
2023-06-05 23:42:37 -0230

    update cabeçalhos

--- Ponteiros/operacoesponteiros2.cpp
+++ Ponteiros/operacoesponteiros2.cpp
@@ -1,6 +1,9 @@
 /* Autor:Walber Mota
  Linguagem: C++
  Criado em 23/05/2023
+ Version 01.00 
+locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM)
+i5-5200U CPU @ 2.20GHz (306D4)
  Objetivo: Operações com ponteiros
 */
 
--- Ponteiros/ponteirogenerico-void.cpp
+++ Ponteiros/ponteirogenerico-void.cpp
@@ -1,6 +1,9 @@
 /*
 Autor: WalberMota
 Data da Criação: 23/05/2023
+Version 01.00 
+locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM)
+i5-5200U CPU @ 2.20GHz (306D4)
 Objetivos: cria um ponteiro void (generico) e obtem o valor de dois tipos diferentes de variáveis.
 Linguagem C++
  */
