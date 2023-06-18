48b63ae04d0933430678d160830223da8192243c
Walber S Mota <mota.csharp@gmail.com>
2023-06-18 19:16:25 -0300

    Atualizando getline.cpp
    cabeçalho

--- Estudos-getline/getline.cpp
+++ Estudos-getline/getline.cpp
@@ -1,3 +1,13 @@
+/*
+Programa:ASCIIextrator.cpp
+Escrito por: Walber Mota
+Linguagem: C++
+Data da Criação: 18 Junho 2023
+Version 01.00 
+locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM)
+i5-5200U CPU @ 2.20GHz (306D4)
+Propósito:  Diferentes formas de obter entradas complexas e com limites definido pelo teclado.
+*/
 #include<iostream>
 #include<string>
 
