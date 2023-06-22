7439657396954a306f9181c73b9e21179f476e6b
Walber S Mota <mota.csharp@gmail.com>
2023-06-22 18:20:37 -0300

    Update datahoraatual.cpp

--- Estudos_ctime/datahoraatual.cpp
+++ Estudos_ctime/datahoraatual.cpp
@@ -1,3 +1,14 @@
+/*
+Programa:
+Escrito por: Walber Mota
+Linguagem: C++
+Data da Criação: 20/06/2023
+Version 01.00 
+locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM)
+i5-5200U CPU @ 2.20GHz (306D4)
+Propósito:  Exibe a data e a hora do sistema.
+*/
+
 #include<iostream>
 #include<ctime>
 
