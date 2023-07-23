15962172aff8d79b085943e0311752b2491c1c30
Walber S Mota <mota.csharp@gmail.com>
2023-07-22 23:22:17 -0300

    criado TamanhoDeCampos.cpp

---
+++ FormatacaoDeCanpos/TamanhoDeCampos.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>//para setw e setprecision
+
+using namespace std;
+
+int main()
+{
+    float lap{1.356},bor{3.242},can{1.558},cad{8.123},pasta{15.408};
+    cout<<"\n\n\n";
+
+    cout << "\n" << "Lapis    "<< setw(10)<< setprecision(3)<<lap;
+    cout << "\n" << "Borracha "<< setw(10)<< setprecision(3)<<bor;
+    cout << "\n" << "Caneta   "<< setw(10)<< setprecision(3)<<can;
+    cout << "\n" << "Caderno  "<< setw(10)<< setprecision(3)<<cad;
+    cout << "\n" << "Pasta    "<< setw(10)<< setprecision(4)<<pasta;
+
+    return 0;
+}
