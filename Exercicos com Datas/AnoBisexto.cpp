8a85e2d342e7e50f0432f1c6a847af56cb993031
Walber S Mota <mota.csharp@gmail.com>
2023-08-03 19:31:50 -0300

    Atualização do cabeçalho

--- Exercicos com Datas/AnoBisexto.cpp
+++ Exercicos com Datas/AnoBisexto.cpp
@@ -6,7 +6,7 @@ Data da Criação: 11/07/2023
 Version 01.00 
 locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM)
 i5-5200U CPU @ 2.20GHz (306D4)
-Propósito:  
+Propósito: Dado um ano qualquer o programa dirá se este ano foi bisexto ou não
 */
 
 #include <iostream>
