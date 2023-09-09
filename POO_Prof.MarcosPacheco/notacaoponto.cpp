98e9040576aacab155bfbcdef144e0781d22f7ff
Walber S Mota <mota.csharp@gmail.com>
2023-09-09 13:19:44 -0300

    update notacaoponto.cpp

--- POO_Prof.MarcosPacheco/notacaoponto.cpp
+++ POO_Prof.MarcosPacheco/notacaoponto.cpp
@@ -10,10 +10,12 @@ Propósito: Estudo de POO com notação ponto(udemy)
 */
 #include <iostream>
 
+void start_option();//definição da função para instruções iniciais
+
 // Criando a classe Casa
 class Casa
 {
-    // public:
+    // quando um atributo é criado na classa ele é por padrão private(privada).
     int NumQuartos{1};
     float Tamanho{50.0f};
     bool bTemSuite{false};
@@ -23,28 +25,41 @@ public:
     int ObtenhaNumeroDeQuartos();
     bool TemSuite();
     void SetNumQuartos(int Num);
+    void SetSuite();
 };
 
+char bsuite;
 int main()
 {
     int numquartos;
-    Casa CasaDePraia;
-    CasaDePraia.MostrarTamanho();
+    Casa CasaDePraia;//instancia um obsjeto da classe Casa
+    start_option();
+
     std::cout << "\nDigite quantos quartos deseja (min=1,max=6)";
     std::cin >> numquartos;
-        CasaDePraia.SetNumQuartos(numquartos);
-    std::cout << "\n Num Quartos: " << CasaDePraia.ObtenhaNumeroDeQuartos();
-    std::cout << "\n Tem Suite: ";
-    CasaDePraia.TemSuite() ? (std::cout << "Sim") : (std::cout << "Nao") << "\n";
-    std::cout << "\n";
+    CasaDePraia.SetNumQuartos(numquartos);
 
-    //system("PAUSE");
+    std::cout << "\nDeseja com suite ? (s/n)";
+    std::cin >> bsuite;
+    CasaDePraia.SetSuite();
+
+    //Exibe resultado final
+    system ("clear");
+    std::cout <<"\nO Aptoº escolhido possui:\n";
+    CasaDePraia.MostrarTamanho();
+    std::cout << "\nNúmero de Quartos: " << CasaDePraia.ObtenhaNumeroDeQuartos();
+    std::cout << "\nSuite: ";
+    CasaDePraia.TemSuite() ? (std::cout << "Sim") : (std::cout << "Nao") << "\n";
+    std::cout << "\n         *** sua reserva foi registrada com sucesso ***";
+    std::cout << "\n                    Agradeçemos a preferência !\n\n\"
+
+    // system("PAUSE");
     return 0;
 }
 
 void Casa::MostrarTamanho()
 {
-    std::cout << "\nTamanho: " << Tamanho;
+    std::cout << "\nTamanho: " << Tamanho << " m²";
 }
 
 int Casa::ObtenhaNumeroDeQuartos()
@@ -60,12 +75,34 @@ bool Casa::TemSuite()
 
 void Casa::SetNumQuartos(int Num)
 {
-        if ((Num <= 6) && (Num >= 1))
+    if ((Num <= 6) && (Num >= 1))
     {
         NumQuartos = Num;
     }
     else
     {
-        std::cout << "\nNumero de Quartos Invalidos, definida para numero padrão\n";
+        std::cout << "\nNumero de Quartos Inválidos, definida para numero padrão\n";
     }
+}
+
+void Casa::SetSuite()
+{
+    if (bsuite == 's' || bsuite == 'S')
+    {
+        bTemSuite = true;
+    }
+    else
+        bTemSuite = false;
+}
+
+void start_option()
+{
+    system ("clear");
+    std::cout << "**********************************\n";
+    std::cout << "1) Escolha o número de quartos\n";
+    std::cout << "2) Escolha se COM ou SEM suite\n";
+    std::cout << "\n";
+    std::cout << "O Tamanho padrão do Aptº é de 50 m²\n";
+    std::cout << "**********************************\n";
+    std::cout << "\n";
 }
