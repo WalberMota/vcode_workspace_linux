338ef84f1d02738155fcbb76560a1bf6d706549a
Walber S Mota <mota.csharp@gmail.com>
2023-09-15 20:13:05 -0300

    update conta.h

---
+++ POO_Prof.MarcosPacheco/conta.cpp
@@ -0,0 +1,7 @@
+/* precisamos incluir o arquivo .h para que o aquivo .cpp possa "Enxergar" as declarações da classe Conta
+ */
+#include "conta.h"
+#include <iostream>
+#include <string>
+
+bool Conta::Sacar(double Valor);
--- POO_Prof.MarcosPacheco/conta.h
+++ POO_Prof.MarcosPacheco/conta.h
@@ -1,26 +1,28 @@
 #pragma once
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
-//no arquivo .h é onde existem as declarações de atributos e métodos (funções) da classe
-
+/*no arquivo .h é onde existem as declarações de atributos e métodos (funções) da classe
+aqui não existe as definições, implementações das funções, pois isto será feito no aquivo .cpp
+ -e a separação do O QUE (.h) do COMO FAZER (.cpp)
+ */
 class Conta
 {
 private:
-    //aqui estão informações que são os atributos e recomendados ficarem privados.
+    // aqui estão informações que são os atributos e recomendados ficarem privados.
     std::string Banco;
     int Agencia;
     int NumConta;
     std::string Titular;
     double Saldo;
 
-public://aqui ficam os métodos (ou funções)
+public: // aqui ficam os métodos (ou funções)
     bool Sacar(double Valor);
     void Depositar(double Valor);
     void Transferir(Conta &Destino, double Valor);
     double ConsultarSaldo();
 
-    //cria as funções para obter os dados privados
+    // cria as funções para obter os dados privados
     std::string GetBanco();
     int GetAgencia();
     int GetNumConta();
@@ -28,6 +30,4 @@ public://aqui ficam os métodos (ou funções)
     void SetBanco(std::string Banco);
     void SetAgencia(int Agencia);
     void SetTitular(std::string Titular);
-
-
 };
