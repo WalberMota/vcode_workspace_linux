cee6a7bec075a7e486f8753a47bf28b297f2e60e
Walber S Mota <mota.csharp@gmail.com>
2023-09-22 20:28:06 -0300

    created jogodaforca.cpp

---
+++ jogos/jogodaforca.cpp
@@ -0,0 +1,23 @@
+/*
+Programa:
+Escrito por: Walber Mota
+Linguagem: C++
+Data da Criação: 18/09/2023
+Version 01.00
+locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM)
+i5-5200U CPU @ 2.20GHz (306D4)
+Propósito:  Simples jogo da Forca.
+*/
+
+
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    char secretword[20],letra[1],palavra[20];
+
+    int tamanho,indice,chances;
+
+
+    return 0;
+}
