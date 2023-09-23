813704532215725f10b4b2f866d90f3c0cb7fa4c
Walber S Mota <mota.csharp@gmail.com>
2023-09-23 18:24:26 -0300

    direitos autorais declarados no cabeçalho

--- jogos/jogodaforca.cpp
+++ jogos/jogodaforca.cpp
@@ -6,7 +6,7 @@ Data da Criação: 18/09/2023
 Version 01.00
 locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM)
 i5-5200U CPU @ 2.20GHz (306D4)
-Propósito:  Simples jogo da Forca.
+Propósito:  Simples jogo da Forca - estudo da codificação do Fessor Bruno (CFB - youtube).
 */
 
 #include <iostream>
