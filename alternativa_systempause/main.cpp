a552f7dfff979a045fbb155e7b661e73762c5e25
Walber S Mota <mota.csharp@gmail.com>
2023-09-25 18:49:21 -0300

    Created main.cppp
    Uma alternativa ao system("pause") no wiindows.

---
+++ alternativa_systempause/main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+int main()
+    {
+
+    int totalfunc{0};
+
+
+    do
+        {
+        std::cout<<"Digite o numero de funcionários\n";
+        std::cin>>totalfunc;
+        if (totalfunc<0)
+            {
+                printf("\n\nNúmero inválido!\n");
+                getchar();
+                scanf("c\n");
+
+                system("clear");
+            }
+        }
+    while(totalfunc<0);
+
+    std::cout<<"\n\ntotal de funcionários: "<<totalfunc;
+
+    return 0;
+
+    }
