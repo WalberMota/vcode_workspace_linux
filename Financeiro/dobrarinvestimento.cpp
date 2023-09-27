460e513b46004a8f57d083166b292b33b7cb476c
Walber S Mota <mota.csharp@gmail.com>
2023-09-27 19:53:04 -0300

    Update dobrarinvestimento.cpp

--- Financeiro/dobrarinvestimento.cpp
+++ Financeiro/dobrarinvestimento.cpp
@@ -1,3 +1,13 @@
+/*
+Programa:
+Escrito por: Walber Mota
+Linguagem: C++
+Data da Criação: 18/09/2023
+Version 01.00
+locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM)
+i5-5200U CPU @ 2.20GHz (306D4)
+Propósito:  Calcula em quantos anos um investimento dobra seu montante a partir de uma determinada taxa anual
+*/  
 ///Falta criar opção para aplicaçõees mensais.
 
 
