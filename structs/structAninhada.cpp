69682a58b5ca0b62594315b97af75473e23f9e06
Walber S Mota <mota.csharp@gmail.com>
2023-10-11 00:40:28 -0300

    update structAninhada.cpp

--- structs/structAninhada.cpp
+++ structs/structAninhada.cpp
@@ -1,3 +1,13 @@
+/*
+Programa:hora.cpp
+Escrito por: Walber S. Mota.
+Linguagem: C++
+Data da Criação: 08 Outubro 2023
+Version 01.00 
+locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM)
+i5-5200U CPU @ 2.20GHz (306D4)
+Propósito:  Estudo com struct aninhadas.
+*/
 #include<iostream>
 #include<cstring>
 
