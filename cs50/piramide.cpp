effa3167c813ea9bca98fe77b1f1fdc08da3061e
Walber S Mota <mota.csharp@gmail.com>
2023-10-12 17:43:36 -0300

    update piramide.cpp - CS50

--- cs50/piramide.cpp
+++ cs50/piramide.cpp
@@ -1,25 +1,23 @@
-#include<iostream>
+#include <iostream>
 #include <unistd.h>
 
-//prototipo das funções
+// prototipo das funções
 int obter_tamanho();
 void piramide(int altura);
 
-//int altura{0};
+// int altura{0};
 
 int main(void)
 {
-    
-    //pega a altura da piramide
-    int altura=obter_tamanho();
 
-    //Função para Construir a piramide
+    // pega a altura da piramide
+    int altura = obter_tamanho();
+
+    // Função para Construir a piramide
     piramide(altura);
-
 
     return 0;
 }
-
 
 int obter_tamanho()
 {
@@ -27,16 +25,39 @@ int obter_tamanho()
     do
     {
         system("clear");
-        std::cout<<"Digite a altura da pirâmide: ";
+        std::cout << "Digite a altura da pirâmide: ";
         std::cin >> n;
 
-    } while (n<=0);
-    
-    return n;
+    } while (n <= 0);
 
+    return n;
 }
 
 void piramide(int size)
 {
+    for (int i = 0; i < size; i++)
+    {
+        /*     for(int j=size;j>=i;j--)
+            {
+                std::cout<<"#";
 
+            } */
+        for (int k = 0; k <= size-i; k++)
+        {
+            std::cout << " ";
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+
+            std::cout << "#";
+        }
+        std::cout << "  ";
+        for (int j = 0; j <= i; j++)
+        {
+            std::cout << "#";
+        }
+
+        std::cout << "\n";
+    }
 }
