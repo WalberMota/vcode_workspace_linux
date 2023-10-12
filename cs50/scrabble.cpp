50a3529dad1838f9516e3b0ef736c0088410bd81
Walber S Mota <mota.csharp@gmail.com>
2023-10-12 19:46:03 -0300

    created scrabble.cpp

---
+++ cs50/scrabble.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+std::string palavra_player1;
+std::string separa;
+
+int main(int argc, char const *argv[])
+{
+    int i{0};
+    std::cout << "Jogador 1\nDigite uma palavra: ";
+    std::cin >> palavra_player1;
+    int tamanho_palavra = palavra_player1.length();
+
+    std::string::iterator it = palavra_player1.begin();
+
+    while (it != palavra_player1.end())
+    {
+        //std::cout << *it << ", ";
+        separa[i]=*it;
+        std::cout<<"separa["<<i<<"]="<<*it<<'\n';
+        i++;
+        ++it;
+
+        //chamar a função que calcula os valores das letras....
+    }
+
+    for (int i=0;i<tamanho_palavra;i++)//mostrar o array
+    {
+        std::cout << separa[i]<<",";
+    }
+    return 0;
+}
