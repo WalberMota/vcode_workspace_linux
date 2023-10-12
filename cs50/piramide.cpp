fbec886fd4fbd10ea8acb7dd10e154875e2c9ea5
Walber S Mota <mota.csharp@gmail.com>
2023-10-12 17:08:54 -0300

    update piramide.cpp

--- cs50/piramide.cpp
+++ cs50/piramide.cpp
@@ -1,17 +1,42 @@
 #include<iostream>
+#include <unistd.h>
 
-int altura{0};
+//prototipo das funções
+int obter_tamanho();
+void piramide(int altura);
 
-int int main(int argc, char const *argv[])
+//int altura{0};
+
+int main(void)
 {
     
+    //pega a altura da piramide
+    int altura=obter_tamanho();
 
-    
-
-
-
-
+    //Função para Construir a piramide
+    piramide(altura);
 
 
     return 0;
+}
+
+
+int obter_tamanho()
+{
+    int n;
+    do
+    {
+        system("clear");
+        std::cout<<"Digite a altura da pirâmide: ";
+        std::cin >> n;
+
+    } while (n<=0);
+    
+    return n;
+
+}
+
+void piramide(int size)
+{
+
 }
