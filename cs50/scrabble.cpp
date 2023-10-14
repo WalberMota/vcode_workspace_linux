96d21c7b18ad8d7690969828bd774fb8f28237c6
Walber S Mota <mota.csharp@gmail.com>
2023-10-14 00:53:33 -0300

    update scrabble.cpp

--- cs50/scrabble.cpp
+++ cs50/scrabble.cpp
@@ -14,7 +14,7 @@ int lenght;
 
 
 // Tabela de Arrays de controle
-int valor_letra[26] = {1, 5, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 2, 1, 4, 4, 8, 4, 6};
+int valor_letra[26] = {2, 3, 3, 2, 1, 4, 2, 6, 1, 8, 5, 1, 3, 1, 2, 3, 7, 1, 1, 2, 3, 4, 4, 7, 4, 6};
 char alfabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 char caixa_baixa;
@@ -33,34 +33,46 @@ int main(int argc, char const *argv[])
     {
         palavra_player[x] = char(tolower(palavra_player[x]));
     }
-    std::cout << "\nEm minuscula= "<< palavra_player<<'\n';
+    //std::cout << "\nEm minuscula= "<< palavra_player<<'\n';
     //------------------------------------------------------
 
     lenght = palavra_player.length();
     //chama a função que calcular o valor da palavra
     soma_player1=indice_letra(lenght);   
-    std::cout <<"\nValor da palavra: " << soma_player1 << '\n';
+    
 
 //jogador 2 ===================================================
-    std::cout << "Jogador 1\nDigite uma palavra: ";
+    std::cout << "\n\nJogador 2\nDigite uma palavra: ";
     std::cin >> palavra_player;
     //passa para minuscula ----------------------------------
     for (int x = 0; x < palavra_player.length(); x++)
     {
         palavra_player[x] = char(tolower(palavra_player[x]));
     }
-    std::cout << "\nEm minuscula= "<< palavra_player<<'\n';
+    //std::cout << "\nEm minuscula= "<< palavra_player<<'\n';
     //-------------------------------------------------------
 
     lenght = palavra_player.length();
     //chama a função que calcular o valor da palavra
     soma_player2=indice_letra(lenght);
     
-    std::cout <<"\nValor da palavra: " << soma_player2 << '\n';
-   
-   //Verifica o vencedor ===========================================
 
-   /* CODE HERE */
+   //Verifica o vencedor ===========================================
+    std::cout <<"\n\nJogador 1: " << soma_player1 <<" pontos"<< '\n';
+    std::cout <<"\nJogador 2: " << soma_player2 <<" pontos"<< '\n';
+    if (soma_player1>soma_player2)
+    {
+        std::cout<<"\n\tJogador 1 é o vencedor!";
+    }
+    if (soma_player1<soma_player2)
+    {
+        std::cout<<"\n\tJogador 2 é o vencedor!";
+    }
+    if (soma_player1==soma_player2)
+    {
+        std::cout<<"\n\tEmpate!";
+    }
+
     return 0;
 }
 
