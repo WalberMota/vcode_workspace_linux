cef7fd0baabc7d7c107bbec8adcb1f7591d3dce0
Walber S Mota <mota.csharp@gmail.com>
2023-10-13 21:37:30 -0300

    update scrabble.cpp

--- cs50/scrabble.cpp
+++ cs50/scrabble.cpp
@@ -8,7 +8,7 @@ int indice_letra(int lenght);
 int soma;
 
 // Tabela de Arrays de controle
-int valor_letra[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 6};
+int valor_letra[26] = {1, 5, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 2, 1, 4, 4, 8, 4, 6};
 char alfabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
