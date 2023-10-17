ec49664154939025531ef1c0ed545bebcfa708be
Walber S Mota <mota.csharp@gmail.com>
2023-10-17 08:06:51 -0300

    update zenitpolar.cpp

--- Operacoes com strings/zenitpolar.cpp
+++ Operacoes com strings/zenitpolar.cpp
@@ -3,40 +3,40 @@
  
 int main()
 {
-    std::string str("The quest for perfection is an endless journey many of us have been on for as long as we can remember. We somehow think our mistakes or shortcomings are failures or make us “less than” rather than viewing them as lessons to be learned unique to us as individuals.");
+    std::string str("A busca pela perfeicao");
  
  for(int i=0;i<str.length();i++)
     switch (str[i])
     {
-    case 'a'|'A':
-        str.replace(i, 1, "4");
-        break;
-    case 'i'|'I':
-        str.replace(i, 1, "1");
-        break;
-    case 'o'|'O':
-        str.replace(i, 1, "0");
-        break;
-    case 's'|'S':
-        str.replace(i, 1, "5");
-        break;
     case 'z'|'Z':
-        str.replace(i, 1, "2");
+        str.replace(i, 1, "p");
         break;
     case 'e'|'E':
-        str.replace(i, 1, "3");
+        str.replace(i, 1, "o");
+        break;
+    case 'n'|'N':
+        str.replace(i, 1, "l");
+        break;
+    case 'i'|'I':
+        str.replace(i, 1, "a");
         break;
     case 't'|'T':
-        str.replace(i, 1, "7");
+        str.replace(i, 1, "r");
         break;
-    case 'b'|'B':
-        str.replace(i, 1, "8");
+    case 'p'|'P':
+        str.replace(i, 1, "z");
         break;
-    case 'q'|'Q':
-        str.replace(i, 1, "9");
+    case 'o'|'O':
+        str.replace(i, 1, "e");
         break;
-    case 'g'|'G':
-        str.replace(i, 1, "6");
+    case 'l'|'L':
+        str.replace(i, 1, "n");
+        break;
+    case 'a'|'A':
+        str.replace(i, 1, "i");
+        break;
+    case 'r'|'R':
+        str.replace(i, 1, "t");
         break;
     default:
         break;
