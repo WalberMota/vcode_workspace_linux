426c2c328c5bba111907d717e451c2aeedceb38c
Walber S Mota <mota.csharp@gmail.com>
2023-10-17 07:36:45 -0300

    update heckertext.cpp

--- Operacoes com strings/hackertext.cpp
+++ Operacoes com strings/hackertext.cpp
@@ -5,9 +5,36 @@ int main()
 {
     std::string str("The quick brown fox jumps over the lazy dog.");
  
-    str.replace(10, 5, "vermelha"); // (4)
+ for(int i=0;i<str.length();i++)
+    switch (str[i])
+    {
+    case 'a':
+        str.replace(i, 1, "4");
+        break;
+    case 'i':
+        str.replace(i, 1, "1");
+        break;
+    case 'o':
+        str.replace(i, 1, "0");
+        break;
+    case 's':
+        str.replace(i, 1, "5");
+        break;
+    case 'z':
+        str.replace(i, 1, "2");
+        break;            
+    default:
+        break;
+    }
+//    if (str[i]=='a')
+//    {
+//
+//    }
+    
  
+/*
     str.replace(str.begin(), str.begin() + 3, 1, 'A'); // (5)
+*/
  
     std::cout << str << '\n';
 }
