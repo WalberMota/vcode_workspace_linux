3ae84a99b19fe5117286f3ca52ac87ac60e22dd4
Walber S Mota <mota.csharp@gmail.com>
2023-10-17 07:55:21 -0300

    update hackertext.cpp

--- Operacoes com strings/hackertext.cpp
+++ Operacoes com strings/hackertext.cpp
@@ -3,26 +3,41 @@
  
 int main()
 {
-    std::string str("The quick brown fox jumps over the lazy dog.");
+    std::string str("The quest for perfection is an endless journey many of us have been on for as long as we can remember. We somehow think our mistakes or shortcomings are failures or make us “less than” rather than viewing them as lessons to be learned unique to us as individuals.");
  
  for(int i=0;i<str.length();i++)
     switch (str[i])
     {
-    case 'a':
+    case 'a'|'A':
         str.replace(i, 1, "4");
         break;
-    case 'i':
+    case 'i'|'I':
         str.replace(i, 1, "1");
         break;
-    case 'o':
+    case 'o'|'O':
         str.replace(i, 1, "0");
         break;
-    case 's':
+    case 's'|'S':
         str.replace(i, 1, "5");
         break;
-    case 'z':
+    case 'z'|'Z':
         str.replace(i, 1, "2");
-        break;            
+        break;
+    case 'e'|'E':
+        str.replace(i, 1, "3");
+        break;
+    case 't'|'T':
+        str.replace(i, 1, "7");
+        break;
+    case 'b'|'B':
+        str.replace(i, 1, "8");
+        break;
+    case 'q'|'Q':
+        str.replace(i, 1, "9");
+        break;
+    case 'g'|'G':
+        str.replace(i, 1, "6");
+        break;
     default:
         break;
     }
