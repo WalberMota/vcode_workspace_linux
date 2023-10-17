4912a88b159df88b81bf5676eaa5079af9fcfba4
Walber S Mota <mota.csharp@gmail.com>
2023-10-17 14:02:20 -0300

    updat hackertext.cpp

--- Operacoes com strings/hackertext.cpp
+++ Operacoes com strings/hackertext.cpp
@@ -1,3 +1,15 @@
+/*
+Programa:
+Escrito por: Walber Mota
+Linguagem: C++
+Data da Criação: 177/10/2023
+Version 01.00 
+locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM)
+i5-5200U CPU @ 2.20GHz (306D4)
+Propósito:  rotina que transcreve um texto para estilo Harcker.
+*/
+
+
 #include <iostream>
 #include <cstring>
 
