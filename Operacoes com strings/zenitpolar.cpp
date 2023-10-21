843b6f30a18e2f73c664b50e08d51119a416e5f2
Walber S Mota <mota.csharp@gmail.com>
2023-10-21 18:28:47 -0300

    update zenitpolar.cpp

--- Operacoes com strings/zenitpolar.cpp
+++ Operacoes com strings/zenitpolar.cpp
@@ -1,3 +1,12 @@
+/* Autor:Walber Mota
+ Linguagem: C++
+ Criado em 23/05/2023
+ Version 01.00 
+locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM)
+i5-5200U CPU @ 2.20GHz (306D4)
+ Objetivo: Transforma um texto qualquer em um encriptação usado a chave ZENIT/POLAR
+*/
+
 #include <iostream>
 #include <string>
 
