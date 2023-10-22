a9a88b01f9f7d8b38336a7a341c20d203c9710a3
Walber S Mota <mota.csharp@gmail.com>
2023-10-22 18:56:54 -0300

    update universidade.cpp

--- POO_Marco Aurélio Regis/Universidade.cpp
+++ POO_Marco Aurélio Regis/Universidade.cpp
@@ -14,9 +14,12 @@ int main(int argc, char const *argv[])
 {
     Aluno *a1=new Aluno(); // o objeto é armazenado na HEAP, quando precisamos do \
                                 objeto por longo período na aplicação.
-    a1->codigo;
+    a1->codigo=100;
 
     Aluno a2;   //o objeto criado é armazenado na STACK (pilha). Aundo a vida dele está delimitada\
                     dentro de um escopo então não precisamos dele muito tempo
+    a2.codigo=100;
+
+    
     return 0;
 }
