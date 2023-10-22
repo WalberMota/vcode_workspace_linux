f8974aec06af7e573df96b7205dfe3dbff3d0410
Walber S Mota <mota.csharp@gmail.com>
2023-10-22 18:42:55 -0300

    criado universidade.cpp

---
+++ POO_Marco Aurélio Regis/Universidade.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+class Aluno{
+
+    int codigo;
+    std::string nome;
+    
+};
+
+
+int main(int argc, char const *argv[])
+{
+    Aluno *a1=new();
+    return 0;
+}
