70af4ad363b57c36225a6cf411d6434a30892717
Walber S Mota <mota.csharp@gmail.com>
2023-10-22 19:27:57 -0300

    update universidade.cpp

--- POO_Marco Aurélio Regis/Universidade.cpp
+++ POO_Marco Aurélio Regis/Universidade.cpp
@@ -35,13 +35,13 @@ int main(int argc, char const *argv[])
 {
     Aluno *a1=new Aluno(); // o objeto é armazenado na HEAP, quando precisamos do \
                                 objeto por longo período na aplicação.
-    a1->codigo=100; //aqui neste caso da HEAP estamos trabalhando com ponteiros.    
+    a1->setCodigo(100); //aqui neste caso da HEAP estamos trabalhando com ponteiros.    
 
     Aluno a2;   //o objeto criado é armazenado na STACK (pilha). Aundo a vida dele está delimitada\
                     dentro de um escopo então não precisamos dele muito tempo
-    a2.codigo=100;
+    a2.setCodigo(100);
 
-std::cout << a1->codigo <<'\n';
+//std::cout << a1->codigo <<'\n';
 
     return 0;
 }
