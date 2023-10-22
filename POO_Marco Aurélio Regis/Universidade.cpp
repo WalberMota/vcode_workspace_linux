565699705ff87c3154aef5207a0552301a297bca
Walber S Mota <mota.csharp@gmail.com>
2023-10-22 18:45:24 -0300

    update universidade.cpp

--- POO_Marco Aurélio Regis/Universidade.cpp
+++ POO_Marco Aurélio Regis/Universidade.cpp
@@ -11,6 +11,7 @@ class Aluno{
 
 int main(int argc, char const *argv[])
 {
-    Aluno *a1=new();
+    Aluno *a1=new Aluno(); // o objeto criado é armazenado na HEAP
+    Aluno a2;   //o objeto criado é armazenado na STACK (pilha)
     return 0;
 }
