4a3a187a3410d520c4b58cd79669a7b89f7fe00f
Walber S Mota <mota.csharp@gmail.com>
2023-10-23 20:35:03 -0300

    update construtores.cpp

--- POO_Marco Aurélio Regis/contrutores.cpp
+++ POO_Marco Aurélio Regis/contrutores.cpp
@@ -3,5 +3,5 @@ Construtores - são métodos que facilitam ao programa a inicialização dos dad
 Um construtor dever possuir o mesmo nome da classe.
 Uma classe pode ter mais de um construtor.
 Um construtor pode ter zero, um ou mais parâmetros.
-
+Um construtor não tem valor de retorno.
 */
