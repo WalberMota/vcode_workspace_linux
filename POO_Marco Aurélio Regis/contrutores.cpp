c544acfad565ddf4449903e39674f2730c1cad02
Walber S Mota <mota.csharp@gmail.com>
2023-10-23 20:35:46 -0300

    update construtores.cpp

--- POO_Marco Aurélio Regis/contrutores.cpp
+++ POO_Marco Aurélio Regis/contrutores.cpp
@@ -4,4 +4,5 @@ Um construtor dever possuir o mesmo nome da classe.
 Uma classe pode ter mais de um construtor.
 Um construtor pode ter zero, um ou mais parâmetros.
 Um construtor não tem valor de retorno.
+Um construtor é sempre chamado quando um objeto pe criado.
 */
