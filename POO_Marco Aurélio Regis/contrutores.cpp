d283e73765af34edd59df36a0cecc58e59e50a1e
Walber S Mota <mota.csharp@gmail.com>
2023-10-23 20:33:13 -0300

    created construtores.cpp

--- POO_Marco Aurélio Regis/contrutores.cpp
+++ POO_Marco Aurélio Regis/contrutores.cpp
@@ -1,3 +1,6 @@
 /*
-Construtores - são métodos que facilitam ao programa a inicialização dos dados-membros da classe    
+Construtores - são métodos que facilitam ao programa a inicialização dos dados-membros da classe  
+Um construtor dever possuir o mesmo nome da classe.
+Uma classe pode ter mais de um construtor.
+  
 */
