fcba73f2e8dc0bcdd80745e58f2ea826ed2a3453
Walber S Mota <mota.csharp@gmail.com>
2023-10-24 20:38:46 -0300

    POO Marcos  Aurelio exercicio com construtor

---
+++ POO_Marco Aurélio Regis/calculadora.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+class Calculadora
+{
+
+private:
+    double res;
+
+public:
+    Calculadora()
+    {
+        res = 0.0;
+    }
+
+    void setRes(double numero)
+    {
+        res = numero;
+    }
+
+    double soma(double num)
+    {
+        res += num;
+        return res;
+    }
+    double subtrai(double num)
+    {
+        res -= num;
+        return res;
+    }
+
+    double multiplica(double num)
+    {
+        res *= num;
+        return res;
+    }
+    double divide(double num)
+    {
+        res /= num;
+        return res;
+    }
+};
+
+int main()
+{
+
+    Calculadora calc;
+    calc.setRes(10.75);
+    std::cout << "soma= " << calc.soma(3.5) << '\n';
+
+    calc.setRes(10.75);    
+    std::cout << "subtrai= " << calc.subtrai(3.5) << '\n';
+
+    calc.setRes(10.75);
+    std::cout << "multiplica = " << calc.multiplica(3.5) << '\n';
+
+    calc.setRes(10.75);
+    std::cout << "divide = " << calc.divide(3.5) << '\n';
+
+    return 0;
+}
