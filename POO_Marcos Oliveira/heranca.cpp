f0c5c5c511707c273a5ea08778a9aed197bab1e1
Walber S Mota <mota.csharp@gmail.com>
2023-10-23 22:34:55 -0300

    update heranca.cpp

--- POO_Marcos Oliveira/heranca.cpp
+++ POO_Marcos Oliveira/heranca.cpp
@@ -2,19 +2,21 @@
 
 class Pessoa{
   public:
-    void nome(){
+    void nome()
+    {
       std::cout << "Marcos Oliveira" << '\n';
     }
-    void idade(){
+    void idade()
+    {
     std::cout << "42 anos" << '\n';
     }
 };
-
  // coloca-se 'public:' e o nome da classe de onde os membros serão herdados
- class Habilidades : public Pessoa{
+ class Habilidades : public Pessoa
+ {
   public:
     void idioma(){
-      std::cout << "Português Brasileiro" << '\n';
+      std::cout << "Programar em C++" << '\n';
     }
 };
 
@@ -22,7 +24,9 @@ int main( int argc , char **argv ){
   Habilidades hab;// é criada um obj chamado hab da classe habilidade
   std::cout << "O nome dele é: ";
   hab.nome(); //a herança possibilita chamar a função-membro da classe pessoa.
-  std::cout << " tem " << hab.idade() << "e A habilidade dele é: ";
+  std::cout << "idade: ";
+  hab.idade();
+  std::cout << "Habilidade dele é: ";
   hab.idioma(); //chama a função-membro próprio da classe habilidade.
   return 0;
 }
