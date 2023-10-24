90647e8424b8e30180e2613f08ef5e1d08a42dbe
Walber S Mota <mota.csharp@gmail.com>
2023-10-23 21:09:49 -0300

    update construtores.cpp

--- POO_Marco Aurélio Regis/contrutores.cpp
+++ POO_Marco Aurélio Regis/contrutores.cpp
@@ -18,7 +18,7 @@ private:
 
 public:
     //Construtor
-    Aluno(int codiigo, std::string nome)
+    Aluno(int codigo, std::string nome)
     {
         this->setCodigo(codigo);
         this->setNome(nome);
@@ -50,9 +50,10 @@ public:
             std::cout << "código não pode ser zero! Tente novamente...";
         }
         else
+        {
             this->codigo=codigo;//o operador this referencia o parâmetro\
                                   implicito de um atributo da classe.
-        
+        }
     }
 
     void setNome(std::string nome)
@@ -65,17 +66,17 @@ public:
 int main(int argc, char const *argv[])
 {
     
-    Aluno *a1=new Aluno(-105,"João"); // devido o construtor quando o objeto\
+    Aluno *a1=new Aluno(11451,"Beltrano"); // devido o construtor quando o objeto\
                                          é criado é neecessário passar parâmetros
 
     std::cout << "\nCódigo aluno a1 = " << a1->getCodigo()<<'\n';
     std::cout << "Nome   aluno a1 = " << a1->getNome()<<'\n';
 
-    Aluno a2(2010,"Maria");// devido o construtor quando o objeto\
+    Aluno a2(23510,"Maria");// devido o construtor quando o objeto\
                               é criado é neecessário passar parâmetros
  
     std::cout << "\nCódigo aluno a2 = " << a2.getCodigo() <<'\n';
     std::cout << "Nome   aluno a2 = " << a2.getNome() <<'\n';
-    //delete(a1);
+    delete(a1);
     return 0;
 }
