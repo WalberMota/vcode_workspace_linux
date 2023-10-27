06d89b24105161bdc12395b544dbe013875b5c16
Walber S Mota <mota.csharp@gmail.com>
2023-10-26 22:32:34 -0300

    update datahoraatual.cpp

--- Estudos_ctime/datahoraatual.cpp
+++ Estudos_ctime/datahoraatual.cpp
@@ -6,7 +6,7 @@ Data da Criação: 20/06/2023
 Version 01.00 
 locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM)
 i5-5200U CPU @ 2.20GHz (306D4)
-Propósito:  Exibe a data e a hora do sistema.
+Propósito:  Exibe a data e a hora do sistema com biblioteca ctime
 */
 
 #include<iostream>
