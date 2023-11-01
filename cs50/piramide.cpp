5ce82cb26bdb76cdb4ea48fd7b0c85b1407e2ede
Walber S Mota <mota.csharp@gmail.com>
2023-10-31 21:49:14 -0300

    update piramide.cpp

--- cs50/piramide.cpp
+++ cs50/piramide.cpp
@@ -1,3 +1,13 @@
+/*
+Programa:datahoraatual.cpp
+Escrito por: Walber Mota
+Linguagem: C++
+Data da Criação: 12/10/2023
+Version 01.00 
+locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM)
+i5-5200U CPU @ 2.20GHz (306D4)
+Propósito:  Cria uma pirâmide no terminal com altura determinada pelo usuário.
+*/
 #include <iostream>
 
 
