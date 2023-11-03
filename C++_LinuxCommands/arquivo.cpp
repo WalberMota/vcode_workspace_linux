d23b695e797f25dc8b16ee9deb924e91dafb4238
Walber S Mota <mota.csharp@gmail.com>
2023-11-03 17:02:02 -0300

    arquivo.cpp

--- C++_LinuxCommands/arquivo.cpp
+++ C++_LinuxCommands/arquivo.cpp
@@ -5,11 +5,15 @@ int main(int argc, char const *argv[])
     FILE* arquivo;
     int inteiro,n1;
     float decimal,n2;
-    char string[100];
+    char string[100], nome[100];
 
-
-
-        
+    printf("Digite o nome do arquivo a ser editado");
+    fgets(nome);
+    
+    //w - apaga todo o conteudo e edita o arquivo
+    //r - le o arquivo
+    //a - ediita mantendo o conteudo
+    arquivo=fopen(nome,"w");
 
     
     return 0;
