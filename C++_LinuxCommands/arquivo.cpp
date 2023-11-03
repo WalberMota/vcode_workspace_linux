0db66a1d6e25a90dea0caf6cac98cdf8ff59f7d5
Walber S Mota <mota.csharp@gmail.com>
2023-11-03 18:15:50 -0300

    update arquivo.cpp

--- C++_LinuxCommands/arquivo.cpp
+++ C++_LinuxCommands/arquivo.cpp
@@ -6,7 +6,7 @@ int main(int argc, char const *argv[])
     //ofstream - sai do programa. (out). Abre o arquivo para receber.
     //ifstream - entra no programa. (in). Abre o arquivo para ler
     //fstram   - pode fazer a abertura do arquivo para ler e escrever.
-
+/* 
     std::ofstream arquivoOut; // armazena no arquivo apagando o conteudo anterior.
     // para adicionar usar:    arquivo.open("ls",std::ofstream::ios_base::app);
     arquivoOut.open("ls");
@@ -14,13 +14,17 @@ int main(int argc, char const *argv[])
     arquivoOut << "2 - Carro\n";
     arquivoOut << "3 - Família\n";
     arquivoOut.close();
-
+ */
     std::ifstream arquivoIn;
     std::string linha;
     arquivoIn.open("ls");
     if(arquivoIn.is_open())
     {
-
+        while (getline(arquivoIn,linha))
+        {
+            std::cout << linha <<'\n';
+        }
+        
 
 
 
@@ -28,7 +32,7 @@ int main(int argc, char const *argv[])
     else
     {
 
-        
+        std::cout << "Não foi possivel abrir o arquivo !"<<'\n';
     }
 
     
