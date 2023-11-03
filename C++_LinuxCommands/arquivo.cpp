8ffc6ffc6d1cf0479ffab2b46b837432657ccb10
Walber S Mota <mota.csharp@gmail.com>
2023-11-03 16:57:01 -0300

    arquivo.cpp

---
+++ C++_LinuxCommands/arquivo.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    FILE* arquivo;
+    int inteiro,n1;
+    float decimal,n2;
+    char string[100];
+
+
+
+        
+
+    
+    return 0;
+}
