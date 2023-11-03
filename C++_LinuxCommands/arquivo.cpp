b282432b74b79abd87507662a872e4e2139ad051
Walber S Mota <mota.csharp@gmail.com>
2023-11-03 17:33:20 -0300

    update arquvo.cpp

--- C++_LinuxCommands/arquivo.cpp
+++ C++_LinuxCommands/arquivo.cpp
@@ -1,51 +1,10 @@
 #include<iostream>
-#include<stdio.h>
+#include<fstream>
+
 
 int main(int argc, char const *argv[])
 {
-    FILE* arquivo;
-    int inteiro,n1;
-    float decimal,n2;
-    char string[100], nome[100],conteudo[100];
-    
-
-    printf("Digite o nome do arquivo a ser editado");
-    std::cin >> nome;
-    
-    //w - apaga todo o conteudo e edita o arquivo
-    //r - le o arquivo
-    //a - edita mantendo o conteudo
-    arquivo=fopen(nome,"w");
-
-    printf("digite um texto para colocar no arquivo: ");
-    fflush(stdin); // limpa o buffer de entrada
-    std::cin>>string;
-
-    fprintf(arquivo,"%s\n",string);
-    
-    printf("Digite um numero inteiro: ");
-    scanf("%d",&inteiro);
-    fprintf(arquivo,"%d", inteiro);
-
-    printf("Digite um numero decimal: ");
-    scanf("%f",&decimal);   
-    fprintf(arquivo,"%f ", decimal);
-    
-    printf("\n\n");
-    arquivo.close();
-
-    //LE ARQUIVO
-    arquivo = fopen(nome,"r");
-    printf("Conteúdo do arquivo");
-
-
-
-
-
-
-
-
-
+    //ofstream - sai do programa e vai para o arquivo
     
     return 0;
 }
