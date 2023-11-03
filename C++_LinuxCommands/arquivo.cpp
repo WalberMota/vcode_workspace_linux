95d322a478636b568ecfe7580d4cda6cecd7dc20
Walber S Mota <mota.csharp@gmail.com>
2023-11-03 17:38:40 -0300

    update arquivo.cpp

--- C++_LinuxCommands/arquivo.cpp
+++ C++_LinuxCommands/arquivo.cpp
@@ -3,8 +3,10 @@
 
 
 int main(int argc, char const *argv[])
-{
-    //ofstream - sai do programa e vai para o arquivo
+{   //tomar como base o programa.
+    //ofstream - sai do programa. (out). Abre o arquivo para receber.
+    //ifstream - entra no programa. (in). Abre o arquivo para ler
+    //fstram   - pode fazer a abertura do arquivo para ler e escrever.
     
     return 0;
 }
