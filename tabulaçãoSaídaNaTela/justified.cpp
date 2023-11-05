fb914d10c2c5e7fa8192ceb10fe647371ad302c1
Walber S Mota <mota.csharp@gmail.com>
2023-11-05 20:33:37 -0300

    update justified.cpp

--- tabulaçãoSaídaNaTela/justified.cpp
+++ tabulaçãoSaídaNaTela/justified.cpp
@@ -15,7 +15,9 @@ int main()
     std::cout << std::right;
 
     std::cout << std::setw(col_width) << "Primeiro Nome" << std::setw(col_width) << "Sobrenome" << std::setw(col_width) << "Idade\n";
-    std::cout << std::setw(col_width) << "Daniel" << std::setw(col_width) << "Gray" << std::setw(col_width) << "29\n";
+    std::cout << std::setw(col_width) << "Daniel" << std::setw(col_width) << "Gray" << std::setw(col_width) << "32\n";
+    std::cout << std::setw(col_width) << "Jordan" << std::setw(col_width) << "Woody" << std::setw(col_width) << "18\n";
+    std::cout << std::setw(col_width) << "Stanley" << std::setw(col_width) << "Park" << std::setw(col_width) << "54\n";    
 
 
     return EXIT_SUCCESS;
