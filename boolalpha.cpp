560e81fe6fd7d33407cf50365c01f741294e330e
Walber S Mota <mota.csharp@gmail.com>
2023-11-04 23:23:05 -0300

    created boolalpha.cpp

---
+++ boolalpha.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    bool condition{true};
+    bool other_condition{false};
+
+    std::cout <<"Condição: " << condition <<'\n';
+    std::cout <<"Outra condição: " << other_condition <<'\n\n';
+
+    
+    std::cout<<std::boolalpha;
+    std::cout <<"Condição: " << condition <<'\n';
+    std::cout <<"Outra condição: " << other_condition <<'\n\n';
+
+    std::cout<<std::noboolalpha;
+    std::cout <<"Condição: " << condition <<'\n';
+    std::cout <<"Outra condição: " << other_condition <<'\n';
+
+
+    return 0;
+}
