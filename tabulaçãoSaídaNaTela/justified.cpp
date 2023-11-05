c689ede4203749d39a9aedbe457b3634dd4276fd
Walber S Mota <mota.csharp@gmail.com>
2023-11-05 20:27:53 -0300

    created justified.cpp

---
+++ tabulaçãoSaídaNaTela/justified.cpp
@@ -0,0 +1,21 @@
+/*
+Autor: Walber Mota
+Data da Criação: 05/11/2023
+Objetivos: Ajuste de saida na tela
+*/
+
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    std::cout << "Justificação de tabela à diireita (default): " << '\n';
+
+    int col_width =20;
+    std::cout << std::right;
+
+    std::cout << std::setw(col_width)
+
+
+    return EXIT_SUCCESS;
+}
