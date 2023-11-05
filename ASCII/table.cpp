32c21d34a83994b9dfa97a0a659474e278b3878b
Walber S Mota <mota.csharp@gmail.com>
2023-11-04 23:09:59 -0300

    update table.cpp

--- ASCII/table.cpp
+++ ASCII/table.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
+#include <iomanip>//para setw e setprecision
+
+
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "DEC CHAR      DEC CHAR      DEC CHAR" << '\n';
+    std::cout << std::right;
+    std::cout << "DEC CHAR"<<'\t'<< "DEC CHAR      DEC CHAR" << '\n';
     for (int i=33;i<64;i++)
     {
-        std::cout <<" "<< i << "   " << char(i) <<"        " << i+32 << "   "\
+        std::cout << i << " " << char(i) << "\t\t" << i+32 << "   "\
         << char(i+32)<<"        " <<i+64 << "   " << char(i+64) << '\n';
 
     }
