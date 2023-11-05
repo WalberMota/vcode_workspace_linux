46a396b341606fdfe9dab8fdc0feea3838746942
Walber S Mota <mota.csharp@gmail.com>
2023-11-04 23:37:45 -0300

    update showpositive.cpp

--- método_manipuladores_stream/showpositive.cpp
+++ método_manipuladores_stream/showpositive.cpp
@@ -8,9 +8,14 @@ int main(int argc, char const *argv[])
     std::cout << "pos_num: " << pos_num << '\n';
     std::cout << "neg_num: " << neg_num << "\n\n";
 
-    std::showpos;
+    std::cout << std::showpos;
     std::cout << "pos_num: " << pos_num << '\n';
     std::cout << "neg_num: " << neg_num << "\n\n";
+
+    std::cout << std::noshowpos;
+    std::cout << "pos_num: " << pos_num << '\n';
+    std::cout << "neg_num: " << neg_num << "\n\n";
+
 
 
     return 0;
