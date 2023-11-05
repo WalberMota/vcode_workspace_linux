4d73384d7c4f4ebb301cb958b104cb8712db9451
Walber S Mota <mota.csharp@gmail.com>
2023-11-04 23:14:47 -0300

    update  table.cpp

--- ASCII/table.cpp
+++ ASCII/table.cpp
@@ -6,10 +6,10 @@
 int main(int argc, char const *argv[])
 {
     std::cout << std::right;
-    std::cout << "DEC CHAR"<<'\t'<< "DEC CHAR      DEC CHAR" << '\n';
+    std::cout << "DEC CHAR\tDEC CHAR\tDEC CHAR" << '\n';
     for (int i=33;i<64;i++)
     {
-        std::cout << i << " " << char(i) << "\t\t" << i+32 << "   "\
+        std::cout << i << "   " << char(i) << "\t\t" << i+32 << "   "\
         << char(i+32)<<"\t\t" <<i+64 << "   " << char(i+64) << '\n';
 
     }
