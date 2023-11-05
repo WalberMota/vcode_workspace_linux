5f5b65a71fc7aa09308fd4dba7ce0b3b4b880e25
Walber S Mota <mota.csharp@gmail.com>
2023-11-05 20:34:55 -0300

    update justified.cpp

--- tabulaçãoSaídaNaTela/justified.cpp
+++ tabulaçãoSaídaNaTela/justified.cpp
@@ -9,7 +9,7 @@ Objetivos: Ajuste de saida na tela
 
 int main()
 {
-    std::cout << "Justificação de tabela à diireita (default): " << '\n';
+    std::cout << "Justificação de tabela à direita (default): " << '\n';
 
     int col_width =20;
     std::cout << std::right;
@@ -17,7 +17,8 @@ int main()
     std::cout << std::setw(col_width) << "Primeiro Nome" << std::setw(col_width) << "Sobrenome" << std::setw(col_width) << "Idade\n";
     std::cout << std::setw(col_width) << "Daniel" << std::setw(col_width) << "Gray" << std::setw(col_width) << "32\n";
     std::cout << std::setw(col_width) << "Jordan" << std::setw(col_width) << "Woody" << std::setw(col_width) << "18\n";
-    std::cout << std::setw(col_width) << "Stanley" << std::setw(col_width) << "Park" << std::setw(col_width) << "54\n";    
+    std::cout << std::setw(col_width) << "Stanley" << std::setw(col_width) << "Park" << std::setw(col_width) << "54\n";
+
 
 
     return EXIT_SUCCESS;
