bb171fe777435e8ebf3b01c88b7d8583141703f2
Walber S Mota <mota.csharp@gmail.com>
2023-11-09 21:54:21 -0300

    funcoesmatematicas.cpp

--- Estudos - Daniel Gakwaya/FuncoesMatematicas/funcoesmatematicas.cpp
+++ Estudos - Daniel Gakwaya/FuncoesMatematicas/funcoesmatematicas.cpp
@@ -6,7 +6,7 @@
     double peso{7.7};
     
     //floor
-    std::cout << "Usando std::floor e std::ceil\n";
+    std::cout << "##### Usando std::floor e std::ceil\n";
     std::cout << peso << " arredondado para baixo: " << std::floor(peso) << '\n';
     //ceil
     std::cout << peso << " arredondado para cima : " << std::ceil(peso) << '\n';
@@ -14,7 +14,7 @@
     //usando função abs
     
     double valor1{-5000};
-    std::cout << "\nUsando std::abs\n";
+    std::cout << "\n##### Usando std::abs\n";
     std::cout << valor1 << " tem valor absoluto (abs) de " << std::abs(valor1) << '\n';
     std::cout << peso << " tem valor absoluto (abs) de " << std::abs(peso) << '\n';
     
