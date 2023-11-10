261f6d17d3c18aed285226b1e4e7b43745486d52
Walber S Mota <mota.csharp@gmail.com>
2023-11-09 21:44:25 -0300

    funcoesmatematicas.cpp

---
+++ Estudos - Daniel Gakwaya/FuncoesMatematicas/funcoesmatematicas.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>     
+
+ int main(int argc, char const *argv[])
+ {
+    double peso{7.7};
+
+    //floor
+    std::cout << std::floor(peso) << '\n';
+       
+    return 0;
+ }
+ 
