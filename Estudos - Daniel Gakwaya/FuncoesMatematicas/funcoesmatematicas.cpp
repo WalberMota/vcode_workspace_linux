9f3efe5aa54fd9f1fe6b687bd4f609bf59efe844
Walber S Mota <mota.csharp@gmail.com>
2023-11-09 21:47:02 -0300

    funcoesmatematicas.cpp

--- Estudos - Daniel Gakwaya/FuncoesMatematicas/funcoesmatematicas.cpp
+++ Estudos - Daniel Gakwaya/FuncoesMatematicas/funcoesmatematicas.cpp
@@ -6,10 +6,10 @@
     double peso{7.7};
 
     //floor
-    std::cout << std::floor(peso) << '\n';
+    std::cout << peso << " arredondado para baixo: " << std::floor(peso) << '\n';
     //ceil
-    std::cout << std::ceil(peso) << '\n';
-    
+    std::cout << peso << " arredondado para cima : " << std::ceil(peso) << '\n';
+
     return 0;
  }
  
