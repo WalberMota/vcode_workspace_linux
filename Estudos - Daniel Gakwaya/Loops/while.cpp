6fb32944c2e4633baf3f93dfc81f350c3d60645b
Walber S Mota <mota.csharp@gmail.com>
2023-11-11 16:17:51 -0300

    update while.cpp

--- Estudos - Daniel Gakwaya/Loops/while.cpp
+++ Estudos - Daniel Gakwaya/Loops/while.cpp
@@ -4,10 +4,10 @@ int main(int argc, char const *argv[])
 {
     const size_t LIMITE{3};
     size_t i{}; //declara o iterator
-    while (i<LIMITE)
+    while (i<LIMITE) // aquii ocorre o teste
     {
         std::cout << i << " : Eu amo c++ \n";
-        i++;
+        ++i;//incrementa o iterator
     }
     
     return 0;
