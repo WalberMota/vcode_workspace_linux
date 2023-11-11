44b65b0bedbb4d2009a0c1d5eec7d1ae5494644d
Walber S Mota <mota.csharp@gmail.com>
2023-11-11 16:13:22 -0300

    update while.cpp

---
+++ Estudos - Daniel Gakwaya/Loops/while.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    const size_t LIMITE{0};
+    size_t i{}; //declara o iterator
+    while (i<LIMITE)
+    {
+        std::cout << i << " : Eu amo c++ \n";
+    }
+    
+
+
+
+    return 0;
+}
