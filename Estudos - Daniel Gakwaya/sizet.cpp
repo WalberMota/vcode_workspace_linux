dffbc2cb4f93570b41a20f67f3a449c205e25e97
Walber S Mota <mota.csharp@gmail.com>
2023-11-11 15:36:14 -0300

    update sizet.cpp

---
+++ Estudos - Daniel Gakwaya/sizet.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    size_t{7};
+    std::cout << "i = " << i <<"(" << sizeof(i) << " bytes)";
+    size_t h{43};
+    std::cout <<"\nh = " << h <<"(" << sizeof(h) << " bytes)";
+
+}
