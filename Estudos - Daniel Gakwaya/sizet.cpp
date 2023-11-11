aea47a0d231e9e61a6536d0fe8fcf5807dff8ef2
Walber S Mota <mota.csharp@gmail.com>
2023-11-11 16:05:30 -0300

    update sizet.cpp

--- Estudos - Daniel Gakwaya/sizet.cpp
+++ Estudos - Daniel Gakwaya/sizet.cpp
@@ -7,4 +7,13 @@ int main(int argc, char const *argv[])
     size_t h{43};
     std::cout <<"\nh = " << h <<"(" << sizeof(h) << " bytes)";
 
+    //uso de size_t em for loop
+
+    for (size_t i{0};i<5;i++)
+    {
+        std::cout << "Eu gosto de c++\n";
+
+    }
+
+    return 0;
 }
