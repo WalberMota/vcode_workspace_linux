86b5de39681590cb5a6378624b1f265fb3d30486
Walber S Mota <mota.csharp@gmail.com>
2023-11-12 17:53:38 -0300

    update while.cpp

--- Estudos - Daniel Gakwaya/Loops/while.cpp
+++ Estudos - Daniel Gakwaya/Loops/while.cpp
@@ -1,3 +1,11 @@
+/*
+Pilar de qualquer loop
+1-Iterator
+2-Ponto de partida
+3-Teste
+4-Incremento ou decremento
+5-Corpo do loop
+*/
 #include<iostream>
 
 int main(int argc, char const *argv[])
@@ -9,6 +17,7 @@ int main(int argc, char const *argv[])
         std::cout << i << " : Eu amo c++ \n";
         ++i;//incrementa o iterator
     }
-    
+    std::cout <<"\n\n" << i;
+
     return 0;
 }
