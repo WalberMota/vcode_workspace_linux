6c5e3c4e9511c4d019a39485d769120d77649ab4
Walber S Mota <mota.csharp@gmail.com>
2023-11-12 18:05:33 -0300

    update dowhileloop.cpp

--- Estudos - Daniel Gakwaya/Loops/dowhileloop.cpp
+++ Estudos - Daniel Gakwaya/Loops/dowhileloop.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char const *argv[])
 {
     const unsigned int LIMITE{10};
-    unsigned int i{11};//o iterator é maior do que a condição LIMTE porem o corpo do loop é executado assim mesmo, ao contrário de while loop onde a condição é testada antes do corpo do loop
+    unsigned int i{11};//o iterator é maior do que a condição LIMTE porem o corpo do loop é executado assim mesmo pois o teste da condição ocorre somente no final, ao contrário de 'loop while' onde a condição é testada, se é verdadeira, antes do corpo do loop.
 
     do
     {
