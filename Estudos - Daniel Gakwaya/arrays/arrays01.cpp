56558674112bab2c76368227d91bc5df392c7522
Walber S Mota <mota.csharp@gmail.com>
2023-11-12 21:57:34 -0300

    update arrays01.cpp

--- Estudos - Daniel Gakwaya/arrays/arrays01.cpp
+++ Estudos - Daniel Gakwaya/arrays/arrays01.cpp
@@ -3,6 +3,10 @@
 int main(int argc, char const *argv[])
 {
     int scores[10]; //array estocando 10 inteiros
+    scores[0]= 20;
+    scores[1]= 21;
+    scores[2]= 22;
+
     for (size_t i = 0; i < 10; i++)
         std::cout << "scores[" << i << "]: " << scores[i] << '\n';
     
