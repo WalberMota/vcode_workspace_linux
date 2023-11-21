c11d7ebb0602bdb166235a55c70feba7dcd18ce8
Walber S Mota <mota.csharp@gmail.com>
2023-11-21 11:54:24 -0300

    update universidadeprj.cpp

--- POO_Marco Aurélio Regis/Projeto_universidade/UniversidadePrj.cpp
+++ POO_Marco Aurélio Regis/Projeto_universidade/UniversidadePrj.cpp
@@ -2,7 +2,7 @@
 // Name        : UniversidadePrj.cpp
 // Author      : 
 // Version     :
-// Copyright   : Your copyright notice
+// Copyright   : Your copyright notice 
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
@@ -15,30 +15,30 @@ using namespace graduacao;
 
 int main() {
 
-	Aluno *a1 = new Aluno(100, "João"); //O objeto a1 é armazenado na área de HEAP em memória.
+	Aluno *a1 = new Aluno(100, "Joï¿½o"); //O objeto a1 Ã© armazenado na Ã¡rea de HEAP em memï¿½ria.
 
 	cout << a1->getCodigo() << " ";
 	cout << a1->getNome() << endl;
 
-	Aluno a2(200, "Maria"); //O objeto a2 é armazenado na área de PILHA (STACK) em memória.
+	Aluno a2(200, "Maria"); //O objeto a2 ï¿½ armazenado na ï¿½rea de PILHA (STACK) em memï¿½ria.
 
 	cout << a2.getCodigo() << " ";
 	cout << a2.getNome() << endl;
 
-	//Utilizando construtor de cópia para criar uma cópia do objeto a2.
+	//Utilizando construtor de cï¿½pia para criar uma cï¿½pia do objeto a2.
 	Aluno x = a2;
 
 	cout << x.getCodigo() << " ";
 	cout << x.getNome() << endl;
 
-	//Chamada ao destrutor da classe para desalocar o objeto em memória
+	//Chamada ao destrutor da classe para desalocar o objeto em memï¿½ria
 	delete a1;
 
-	//Calula média notas de Maria
+	//Calula mï¿½dia notas de Maria
 	float notas_maria[2] = {10.0, 8.5};
 	a2.setNotas(notas_maria);
 
-	cout << "Média das notas de Maria é " << a2.calculaMedia() << endl;
+	cout << "Mï¿½dia das notas de Maria ï¿½ " << a2.calculaMedia() << endl;
 
 	return 0;
 }
