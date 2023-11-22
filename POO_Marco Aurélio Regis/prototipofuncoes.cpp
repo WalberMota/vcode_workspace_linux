caf3131fe994ca62118fca8b873967a0bb555830
Walber S Mota <mota.csharp@gmail.com>
2023-11-22 15:45:53 -0300

    created prototipofuncoes.cpp

---
+++ POO_Marco Aurélio Regis/prototipofuncoes.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+
+class Aluno{
+
+private:
+    int codigo;
+    std::string nome;
+
+    float notas[2]{0.0,0.0};
+
+public:
+    
+    Aluno(int codigo, std::string nome)
+    {
+        this -> setCodigo(codigo);
+        this -> setNome(nome);
+
+        std::cout << "objeto criado\n";
+    }
+    ~Aluno(){}
+
+    int getCodigo()
+    {
+        return codigo;
+    }
+    std::string getNome()
+    {
+        return nome;
+    }
+    //Setters - as vezes chamados "mutator methods" em algumas literaturas.
+    // aqui criaremos dois setters - um para o atributo 'codigo' e outro para o atributo 'nome'
+    //geralmente 'void' porque somente atribui um valor para um atributo da classe.
+    void setCodigo(int codigo)
+    {
+        if (codigo<0)//o encapsulamento isolou o atributo 'codigo' \
+                        e permitiu criar uma regra de negócio - código não pode ser negativo
+        {
+            codigo *=(-1);
+        }
+        if (codigo==0)
+        {
+            std::cout << "código não pode ser zero! Tente novamente...";
+        }
+        else
+        {
+        this->codigo=codigo;//o operador this referencia o parâmetro implicito de um atributo da classe.
+        }
+    }
+
+    void setNome(std::string nome)
+    {
+        this->nome=nome;
+    }
+};
+
+
+int main(int argc, char const *argv[])
+{
+    Aluno *a1=new Aluno(5050,"Jose"); // aqui o objeto é armazenado na HEAP, quando precisamos do \
+                                objeto por longo período na aplicação. Quando não for mais útil\
+                                teremos que usar 'delete' para liberar da memória.
+    //somente altera o atributo codigo usando o método setCodigo (encapsulamento).
+    a1->setCodigo(0); //aqui neste caso da HEAP estamos trabalhando com ponteiros.    
+
+    std::cout << "Código aluno a1 = " << a1->getCodigo()<<'\n';
+
+    Aluno a2();   //aqui o objeto criado é armazenado na STACK (pilha). Quaundo a vida dele está delimitada\
+                    dentro de um escopo então não precisamos dele muito tempo (deleção automatica).
+    a2.setCodigo(100);
+
+    std::cout << "Código aluno a2 = " << a2.getCodigo() <<'\n';
+
+    return 0;
+}
