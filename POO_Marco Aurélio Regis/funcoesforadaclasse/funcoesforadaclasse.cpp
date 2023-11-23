c805376445659f157f47ac7a84d94578da610536
Walber S Mota <mota.csharp@gmail.com>
2023-11-23 09:58:55 -0300

    created funcoesforadaclasse.cpp

--- POO_Marco Aurélio Regis/funcoesforadaclasse/funcoesforadaclasse.cpp
+++ POO_Marco Aurélio Regis/funcoesforadaclasse/funcoesforadaclasse.cpp
@@ -10,7 +10,8 @@ private:
     //float notas[2]{0.0,0.0};
 
 public:
-    //construtor com dois parâmetros
+
+
     Aluno(int codigo, std::string nome)
     {
         this -> setCodigo(codigo);
@@ -29,16 +30,13 @@ public:
     {
         return nome;
     }
-    //========================
-    //Setters - as vezes chamados "mutator methods" em algumas literaturas.
-    // aqui criaremos dois setters - um para o atributo 'codigo' e outro para o atributo 'nome'
-    //geralmente 'void' porque somente atribui um valor para um atributo da classe.
+
     void setCodigo(int codigo)
     {
-        //o encapsulamento isolou o atributo 'codigo' e permitiu criar uma regra de negócio
+
         if (codigo<0)
         {
-            codigo *=(-1);//transforam um valor negativo em positivo
+            codigo *=(-1);
         }
         if (codigo==0)
         {
@@ -46,8 +44,7 @@ public:
         }
         else
         {
-        this->codigo=codigo;//o operador this referencia o parâmetro implicito de um atributo da classe.
-        }
+        this->codigo=codigo;
     }
 
     void setNome(std::string nome)
@@ -59,16 +56,16 @@ public:
 
 int main(int argc, char const *argv[])
 {
-     Aluno *a1=new Aluno(5050,"Jose"); // HEAP(longo período),o construtor parametrizado exigiu que fossem passados parâmetros quando o objeto foi criado.(acesso pela notação seta)
+     Aluno *a1=new Aluno(5050,"Jose");
     a1->setCodigo(20345);
 
     std::cout << "Nome do aluno a1 = " << a1->getNome()   << '\n';
     std::cout << "Código aluno  a1 = " << a1->getCodigo() << '\n';
 
-    delete a1;//o destrutor é necessário pois o objeto está na HEAP.
+    delete a1;
     std::cout << "manualmente\n";
 
-     Aluno a2(1010,"Marcos");   //STACK (pilha)(deleção automatica) (acesso pela notação ponto)
+     Aluno a2(1010,"Marcos");
     a2.setCodigo(-100);
 
     std::cout << "Nome   aluno a2 = " << a2.getNome();
