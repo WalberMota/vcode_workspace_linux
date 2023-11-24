c41f36bd289830f7592d795dee6355394c9004e0
Walber S Mota <mota.csharp@gmail.com>
2023-11-24 20:44:43 -0300

    update  funcoesforadaclasse.cpp

--- POO_Marco Aurélio Regis/funcoesforadaclasse/funcoesforadaclasse.cpp
+++ POO_Marco Aurélio Regis/funcoesforadaclasse/funcoesforadaclasse.cpp
@@ -10,7 +10,7 @@ private:
     float notas[2]{0.0, 0.0};
 
 public:
-// ******* protótipos das funções *********
+    // ******* protótipos das funções *********
     // contrutor e destrutor
     Aluno(int codigo, std::string nome);
     ~Aluno();
@@ -21,7 +21,7 @@ public:
     void setNome(std::string nome);
     void setCodigo(int codigo);
     void setNotas(float *notas);
-    //método que calcula a média das notas.
+    // método que calcula a média das notas.
     float calculaMedia();
 };
 
@@ -43,4 +43,71 @@ int main(int argc, char const *argv[])
     std::cout << "\nCódigo aluno a2 = " << a2.getCodigo();
 
     return 0;
+}
+
+Aluno(int codigo, std::string nome)
+{
+    this->setCodigo(codigo);
+    this->setNome(nome);
+
+    std::cout << "objeto criado";
+}
+~Aluno()
+{
+    std::cout << "\nObjeto destruído ";
+}
+// gets
+int getCodigo()
+{
+    return codigo;
+}
+
+std::string getNome()
+{
+    return nome;
+}
+
+float *getNotas()
+{
+    return notas;
+}
+
+// setters
+void setNome(std::string nome)
+{
+    this->nome = nome;
+}
+
+void setCodigo(int codigo)
+{
+    if (codigo < 0)
+    {
+        codigo *= (-1);
+    }
+    if (codigo == 0)
+    {
+        std::cout << "código não pode ser zero! Tente novamente...";
+    }
+    else
+    {
+        this->codigo = codigo;
+    }
+}
+void setNotas(float *notas)
+{
+    this->notas[0] = notas[0];
+    this->notas[1] = notas[1];
+}
+
+float calculaMedia()
+{
+    float media = 0.0f;
+
+    float *p = &notas[0];
+    media = media + *p;
+    p++;
+    mediia = mediia + *p;
+    p++;
+    media = media + *p;
+    return media / 2;
 }
