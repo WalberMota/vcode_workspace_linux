7494488a12945d8fbf65b447160b9b446f2a8b2e
Walber S Mota <mota.csharp@gmail.com>
2023-11-24 20:18:37 -0300

    update  funcoesforadaclasse.cpp

--- POO_Marco Aurélio Regis/funcoesforadaclasse/funcoesforadaclasse.cpp
+++ POO_Marco Aurélio Regis/funcoesforadaclasse/funcoesforadaclasse.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 #include <string>
 
-class Aluno{
-
+class Aluno
+{
 private:
     int codigo;
     std::string nome;
 
-    //float notas[2]{0.0,0.0};
+    // float notas[2]{0.0,0.0};
 
 public:
 
+    Aluno(int codigo, std::string nome);
+    ~Aluno();
+
+
     Aluno(int codigo, std::string nome)
     {
-        this -> setCodigo(codigo);
-        this -> setNome(nome);
+        this->setCodigo(codigo);
+        this->setNome(nome);
 
         std::cout << "objeto criado";
-
     }
-
     ~Aluno()
     {
         std::cout << "\nObjeto destruído ";
-    
     }
-
+    //gets
     int getCodigo()
     {
         return codigo;
@@ -35,42 +36,42 @@ public:
         return nome;
     }
 
+    // setters
+    void setNome(std::string nome)
+    {
+        this->nome = nome;
+    }
 
     void setCodigo(int codigo)
     {
-
-        if (codigo<0)
+        if (codigo < 0)
         {
-            codigo *=(-1);
+            codigo *= (-1);
         }
-        if (codigo==0)
+        if (codigo == 0)
         {
             std::cout << "código não pode ser zero! Tente novamente...";
         }
         else
         {
-        this->codigo=codigo;
-    }
-    void setNome(std::string nome)
-    {
-        this->nome=nome;
+            this->codigo = codigo;
+        }
     }
 
 };
 
-
 int main(int argc, char const *argv[])
 {
-     Aluno *a1=new Aluno(5050,"Jose");
+    Aluno *a1 = new Aluno(5050, "Jose");
     a1->setCodigo(20345);
 
-    std::cout << "Nome do aluno a1 = " << a1->getNome()   << '\n';
+    std::cout << "Nome do aluno a1 = " << a1->getNome() << '\n';
     std::cout << "Código aluno  a1 = " << a1->getCodigo() << '\n';
 
     delete a1;
     std::cout << "manualmente\n";
 
-     Aluno a2(1010,"Marcos");
+    Aluno a2(1010, "Marcos");
     a2.setCodigo(-100);
 
     std::cout << "Nome   aluno a2 = " << a2.getNome();
