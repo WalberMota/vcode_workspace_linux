d9b3edce262ad7e146c426dd66a79fb80901d3d1
Walber S Mota <mota.csharp@gmail.com>
2023-11-26 13:31:40 -0300

    update investimento.cpp

--- Matematica/Investimento/investimento.cpp
+++ Matematica/Investimento/investimento.cpp
@@ -9,6 +9,6 @@ int main(int argc, char const *argv[])
         inicial=inicial+(inicial*1.05);
         i++;
     } while (inicial<20000);
-    
+    std::cout << "o valor de 10000 com redimento de 5% a.a. dobrará de valor em " << i;
     return 0;
 }
