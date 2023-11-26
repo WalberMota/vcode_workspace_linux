c8477e04eff13da0c14b4bc9836ee81122709aa7
Walber S Mota <mota.csharp@gmail.com>
2023-11-26 14:10:28 -0300

    update investimento.cpp

--- Matematica/Investimento/investimento.cpp
+++ Matematica/Investimento/investimento.cpp
@@ -2,10 +2,10 @@
 
 int main(int argc, char const *argv[])
 {
-    float inicial{10000.00};
+    float inicial{1000.00};
     int i=0;
     
-    while (inicial <20000.00)
+    while (inicial <2000.00)
     {
         std::cout << inicial <<" - ano " << i <<'\n';
         inicial=(inicial*1.05);
