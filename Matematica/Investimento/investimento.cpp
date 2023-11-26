91ce034e299cf75afda881d874c792fbf3a69c61
Walber S Mota <mota.csharp@gmail.com>
2023-11-26 14:13:28 -0300

    update investimento.cpp

--- Matematica/Investimento/investimento.cpp
+++ Matematica/Investimento/investimento.cpp
@@ -1,21 +1,18 @@
-#include<iostream>
+#include <iostream>
 
 int main(int argc, char const *argv[])
 {
     float inicial{1000.00};
-    int i=0;
-    
-    while (inicial <2000.00)
+    int i = 0;
+
+    while (inicial < 2000.00)
     {
-        std::cout << inicial <<" - ano " << i <<'\n';
-        inicial=(inicial*1.05);
+        std::cout << "ano " << i << "\t R$" << inicial << '\n';
+        inicial = (inicial * 1.05);
         i++;
     }
-    
 
-
-    
-    //std::cout << "o valor de 10000 com redimento de 5% a.a. dobrará de valor em " << i << " anos\n";
-    //std::cout << "valor final = " << inicial;
+    // std::cout << "o valor de 10000 com redimento de 5% a.a. dobrará de valor em " << i << " anos\n";
+    // std::cout << "valor final = " << inicial;
     return 0;
 }
