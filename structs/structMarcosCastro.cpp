c7e51bf7b56c7e86ea66d699df57c45aca7eb5ba
Walber S Mota <mota.csharp@gmail.com>
2023-11-29 16:16:15 -0300

    update strucMarcos Castro.cpp

--- structs/structMarcosCastro.cpp
+++ structs/structMarcosCastro.cpp
@@ -8,11 +8,12 @@ typedef struct pessoa //define t_pessoa como um alias para a struct pessoa.
 	char nome[100];
 	int idade;
 	int CPF;
+
 } t_pessoa;
 
 int main(int argc, char *argv[])
 {
-	t_pessoa pessoas[100];
+	//t_pessoa pessoas[100];
 	t_pessoa* p;
 
 	p = &pessoas[0];
