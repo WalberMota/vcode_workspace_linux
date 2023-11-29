c2113886f5781a155b6994035865dd78a33d5e41
Walber S Mota <mota.csharp@gmail.com>
2023-11-29 00:23:05 -0300

    update argcargv.cpp

--- argcargv/argcargv.cpp
+++ argcargv/argcargv.cpp
@@ -6,11 +6,11 @@ int main(int argc, char const *argv[])
     if (argc - 1 != 0)
     {
         std::cout << "Quantidades de argumentos passados: " << argc - 1 << '\n';
-        std::cout << "argumentos:\n";        
+        //std::cout << "argumentos:\n";        
         // arg-1 para evitar considerar o proprio programa como argumento
         for (int i = 1; i <= argc - 1; i++) 
         {
-            std::cout << i << "-" << argv[i] << '\n';
+            std::cout <<"Argumento " <<i << " - " << argv[i] << '\n';
         }
     }
     else{std::cout << "\n    nenhum argumento foi passado";}
