b90373be47ec6eb231001ace256385c365fc4f71
Walber S Mota <mota.csharp@gmail.com>
2023-12-05 20:13:52 -0300

    update operadoresmath.cpp

--- C/math/operadoresmath.c
+++ C/math/operadoresmath.c
@@ -2,14 +2,14 @@
 
 int main(void)
 {
-    int numero1,numero2;
+    float numero1,numero2;
     printf("Digite um primeiro número:\n");
-    scanf("%d",&numero1);
+    scanf("%f",&numero1);
     printf("Digite um segundo número:\n");
-    scanf("%d",&numero2);
+    scanf("%f",&numero2);
     printf("\n");
-    printf("A Soma de %d com %d é %d",numero1,numero2,numero1+numero2);
-    printf("A Multiplicação de %d com %d é %d",numero1,numero2,numero1*numero2);
-    printf("A Divisão de %d por %d é %d",numero1,numero2,numero1/numero2);
+    printf("A Soma de %.2f com %.2f é %.2f\n",numero1,numero2,numero1+numero2);
+    printf("A Multiplicação de %.2f com %.2f é %.2f\n",numero1,numero2,numero1*numero2);
+    printf("A Divisão de %.2f por %.2f é %.2f\n",numero1,numero2,numero1/numero2);
     return 0;
 }
