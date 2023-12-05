b891ddba17c48beecca8e27545446580acacebf9
Walber S Mota <mota.csharp@gmail.com>
2023-12-04 23:37:52 -0300

    update operadoresmath.cpp

--- C/math/operadoresmath.c
+++ C/math/operadoresmath.c
@@ -3,9 +3,13 @@
 int main(void)
 {
     int numero1,numero2;
-    printf("Digite um primeiro número \n");
+    printf("Digite um primeiro número:\n");
     scanf("%d",&numero1);
-    printf("Digite um segundo número \n");
+    printf("Digite um segundo número:\n");
     scanf("%d",&numero2);
+    return 0;
+    printf("\n");
+    printf("A Soma de %d com %d é",numero1+numero2);
+
     return 0;
 }
