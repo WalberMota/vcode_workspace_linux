e0a2557ce3a144893c3eb2a494c6d59663ce7a14
Walber S Mota <mota.csharp@gmail.com>
2023-12-08 18:00:23 -0300

    created namespace.cpp

---
+++ POO_Marco Aurélio Regis/namespace/namespace.cpp
@@ -0,0 +1,9 @@
+/*  namespace - palavra reservada da linguagem c++ utilizada para prevenir conflitos
+    entre nome de classe, variáveis e funções em grnades projetos:
+    Possibilita agrupar classes, objetos e funções sob um nome 
+    sintaxe:
+    namespace <nome_namespace>
+    {
+        //código das declarações
+    }
+*/
