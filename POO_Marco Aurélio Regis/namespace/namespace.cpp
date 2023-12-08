f5dda4d9c8bff65401e1fa71028e11b4a64430f3
Walber S Mota <mota.csharp@gmail.com>
2023-12-08 18:04:59 -0300

    update namespace folder

--- POO_Marco Aurélio Regis/namespace/namespace.cpp
+++ POO_Marco Aurélio Regis/namespace/namespace.cpp
@@ -7,3 +7,21 @@
         //código das declarações
     }
 */
+#include <iostream>
+#include <string>
+#include"aluno.h"
+
+//entry point ***********************
+int main(int argc, char const *argv[])
+{
+ 
+    Aluno a(1010, "João");
+
+    std::cout << "Nome   aluno a = " << a.getNome();
+    std::cout << "\nCódigo aluno a = " << a.getCodigo();
+
+	float n[2]{10.0, 8.5};
+	a.setNotas(n);
+	std::cout << "\nA Média das notas é " << a.calculaMedia() << '\n';
+    return 0;
+}
