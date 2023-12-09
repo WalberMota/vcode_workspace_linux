13d246ee2564c19e499beaca3d0341de2dfb4500
Walber S Mota <mota.csharp@gmail.com>
2023-12-09 20:02:26 -0300

    created funcaoAmiga folder

---
+++ POO_Marco Aurélio Regis/funçãoAmiga/aluno.cpp
@@ -0,0 +1,72 @@
+/* aqui o arquivo aluno.cpp contém como as funções são desempenhadas
+  ou seja, a implementação dos métodos da classe aluno
+ */
+
+#include <iostream>
+#include "aluno.h"
+
+//function implementation =====================
+graduacao::Aluno::Aluno(int codigo, std::string nome)
+{
+    this->setCodigo(codigo);
+    this->setNome(nome);
+
+    std::cout << "objeto criado\n";
+}
+graduacao::Aluno::~Aluno()
+{
+    std::cout << "Objeto destruído ";
+}
+// getters
+int graduacao::Aluno::getCodigo()
+{
+    return codigo;
+}
+
+std::string graduacao::Aluno::getNome()
+{
+    return nome;
+}
+
+float *graduacao::Aluno::getNotas()
+{
+    return notas;
+}
+
+// setters
+void graduacao::Aluno::setNome(std::string nome)
+{
+    this->nome = nome;
+}
+
+void graduacao::Aluno::setCodigo(int codigo)
+{
+    if (codigo < 0)
+    {
+        codigo *= (-1);
+    }
+    if (codigo == 0)
+    {
+        std::cout << "código não pode ser zero! Tente novamente...";
+    }
+    else
+    {
+        this->codigo = codigo;
+    }
+}
+void graduacao::Aluno::setNotas(float *notas)
+{
+    this->notas[0] = notas[0];
+    this->notas[1] = notas[1];
+}
+
+float graduacao::Aluno::calculaMedia()
+{
+    float media = 0.0f;
+
+    float *p = &notas[0];
+    media = media + *p;
+    p++;
+    media = media + *p;
+    return media / 2;
+}
---
+++ POO_Marco Aurélio Regis/funçãoAmiga/funcaoamiga.cpp
@@ -0,0 +1,30 @@
+/*  namespace - palavra reservada da linguagem c++ utilizada para prevenir conflitos
+    entre nome de classe, variáveis e funções em grnades projetos:
+    Possibilita agrupar classes, objetos e funções sob um nome 
+    sintaxe:
+    namespace <nome_namespace>
+    {
+        //código das declarações
+    }
+*/
+#include <iostream>
+#include <string>
+#include"aluno.h"
+
+//elimina a necessidade de repetir dentro do código o operador resolução de escopo
+//using namespace graduacao;
+
+//entry point ***********************
+int main(int argc, char const *argv[])
+{
+ 
+    graduacao::Aluno a(1010, "João");
+
+    std::cout << "Nome   aluno a = " << a.getNome();
+    std::cout << "\nCódigo aluno a = " << a.getCodigo();
+
+	float n[2]{10.0, 8.5};
+	a.setNotas(n);
+	std::cout << "\nA Média das notas é " << a.calculaMedia() << '\n';
+    return 0;
+}
