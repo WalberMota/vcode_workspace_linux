efcdb88b8271900e39f14ac0c0468dddbb45c250
Walber S Mota <mota.csharp@gmail.com>
2023-12-09 20:11:32 -0300

    created professor.h

--- POO_Marco Aurélio Regis/funçãoAmiga/professor.h
+++ POO_Marco Aurélio Regis/funçãoAmiga/professor.h
@@ -1,4 +1,22 @@
 #ifndef PROFESSOR_H_
 #define PROFESSOR_H_
 
+class professor
+{
+private:
+    /* data */
+public:
+    professor(/* args */);
+    ~professor();
+};
+
+professor::professor(/* args */)
+{
+}
+
+professor::~professor()
+{
+}
+
+
 #endif /* PROFESSOR_H_ */
