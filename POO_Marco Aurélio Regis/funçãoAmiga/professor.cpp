3333d31a09c6139f8c84a1ba2ecb14600e519977
Walber S Mota <mota.csharp@gmail.com>
2023-12-09 21:12:36 -0300

    update professor.cpp

--- POO_Marco Aurélio Regis/funçãoAmiga/professor.cpp
+++ POO_Marco Aurélio Regis/funçãoAmiga/professor.cpp
@@ -8,7 +8,7 @@ professor::~professor()
 {
 }
 
-void professor::alteraNotaAlunoGraduacao(Aluno a,float n1,float n2)
+void professor::alteraNotaAlunoGraduacao(graduacao::Aluno a,float n1,float n2)
 {
     a.notas[0]=n1;
     a.notas[1]=n2;
