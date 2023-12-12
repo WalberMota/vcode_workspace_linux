2ae9c2048308a43ada578c3e7b4bd4fbfbc59a71
Walber S Mota <mota.csharp@gmail.com>
2023-12-12 20:59:24 -0300

    created decrescente.c

---
+++ C/recursão/decrescente.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int imprimirRecursivo(x)
+{
+    if (x == 0)
+        printf("%d, ",x);
+    else
+    {
+
+    };
+
+
+
+};
+
+
+int main(void)
+{
+    
+
+    printf("tecle ENTER para continuar.");
+    getchar();
+
+    return 0;
+}
