c618f50606ca6c4394430d08679c786b7ea54a00
Walber S Mota <mota.csharp@gmail.com>
2023-12-12 21:09:55 -0300

    Update decrescente.c

--- C/recursão/decrescente.c
+++ C/recursão/decrescente.c
@@ -1,25 +1,33 @@
+/*
+Programa:hora.cpp
+Linguagem: C
+Data da Criação: 12 Dezembo 2023
+Version 01.00 
+locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM)
+i5-5200U CPU @ 2.20GHz (306D4)
+Propósito:  Estudo de funções recursivas.
+*/
+
 #include<stdio.h>
 
-int imprimirRecursivo(x)
+
+int imprimirRecursivo(int x)
 {
     if (x == 0)
-        printf("%d, ",x);
+        printf("%d",x);
     else
     {
-
+        printf("%d, ",x);
+        imprimirRecursivo(x-1);
     };
-
-
 
 };
 
 
 int main(void)
 {
-    
 
-    printf("tecle ENTER para continuar.");
-    getchar();
+    imprimirRecursivo(30);
 
     return 0;
 }
