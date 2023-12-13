f2846da08600f6f9e24fbef8b1c377eb7adb0cd0
Walber S Mota <mota.csharp@gmail.com>
2023-12-13 19:20:20 -0300

    created atribstatic.cpp

---
+++ POO_Marco Aurélio Regis/atributo_estatic/atribstatic.cpp
@@ -0,0 +1,6 @@
+/* Um atributo com o modificador STATIC tem o seu valor compartilhado por
+    os objetos da classe, ou seja, só haverá uma cópia dele para todos os
+    objetos da classe.
+ */
+
+    
