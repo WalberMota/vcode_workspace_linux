c8b4e9c710bc9d8c0fd7a667febb91b11bd6303e
Walber S Mota <mota.csharp@gmail.com>
2023-12-16 11:07:27 -0300

    created atribstatic.cpp

--- POO_Marco Aurélio Regis/atributo_estatic/atribstatic.cpp
+++ POO_Marco Aurélio Regis/atributo_estatic/atribstatic.cpp
@@ -27,7 +27,9 @@ int main(int argc, char const *argv[])
     graduacao::Aluno a2("Maria");
     graduacao::Aluno z("Carlos");
     
-    a.imprimeSaudacao();
+    graduacao::Aluno::imprimeSaudacao();
+    //pode ser usado dentro de qualquer objeto criado, p.ex.: a2.imprimeSaudacao();
+
     std::cout << "aluno: " << a.getNome() << " ";
     std::cout << "\tcódigo :" << a.getCodigo() << '\n';
     std::cout << "aluno: " << a2.getNome() << " ";
