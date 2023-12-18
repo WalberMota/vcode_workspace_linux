7d537839b5ed2691467672a1eddcb71e258cc14c
Walber S Mota <mota.csharp@gmail.com>
2023-12-17 22:35:40 -0300

    update funcoesmatematicas.cpp

--- Estudos - Daniel Gakwaya/FuncoesMatematicas/funcoesmatematicas.cpp
+++ Estudos - Daniel Gakwaya/FuncoesMatematicas/funcoesmatematicas.cpp
@@ -1,3 +1,12 @@
+/*
+Autor: WalberMota
+Data da Criação: 09/11/2023
+Version 01.00 
+locale=en_US.UTF-8,Utf16=on,HugeFiles=on,64 bits,4 CPUs Intel(R) Core(TM) i5-5200U CPU @ 2.20GHz (306D4)
+Objetivos: resumo de funções matemáticas.
+Linguagem C++
+ */
+
 #include<iostream>
 #include<cmath>     
 
