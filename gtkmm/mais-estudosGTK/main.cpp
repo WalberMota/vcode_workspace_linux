b04906902e2995d7a10636bb8732e243cba6eb37
Walber S Mota <mota.csharp@gmail.com>
2023-12-22 16:23:53 -0300

    updte main.cpp(gtkmm)

--- gtkmm/mais-estudosGTK/main.cpp
+++ gtkmm/mais-estudosGTK/main.cpp
@@ -3,6 +3,9 @@
 int main(int argc, char const *argv[])
 {
     auto app = Gtk::Application::create("org.pass.gtkmm");
+    janela  senha;
+
+    return app->make_window_and_run<senha>(argc, argv);
     return 0;
 }
 
