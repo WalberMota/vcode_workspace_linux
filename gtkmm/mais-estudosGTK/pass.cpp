57a76df7af07ac363d71790c5f16d3bebb2eabf4
Walber S Mota <mota.csharp@gmail.com>
2023-12-22 17:07:35 -0300

    update pass.cpp(gtkmm)

--- gtkmm/mais-estudosGTK/pass.cpp
+++ gtkmm/mais-estudosGTK/pass.cpp
@@ -1,9 +1,33 @@
 #include "pass.h"
+#include <iostream>
 
 janela::janela()
 {
     set_title("Gerenciador de Senha - em construção(22/12/2023)");
     set_default_size(500, 300);
     set_visible(true);
+    Gtk::Button m_botao1("Ola,usuário!");// cria o objeto botao com o rótulo "Ola,usuário!"
+  m_button.set_margin(10);
+
+
+
+
+
+
 
 }
+
+
+{
+  // Sets the margin around the button.
+  m_button.set_margin(10);
+
+  // When the button receives the "clicked" signal, it will call the
+  // on_button_clicked() method defined below.
+  m_button.signal_clicked().connect(sigc::mem_fun(*this,
+              &HelloWorld::on_button_clicked));
+
+  // This packs the button into the Window (a container).
+  set_child(m_button);
+}
+
