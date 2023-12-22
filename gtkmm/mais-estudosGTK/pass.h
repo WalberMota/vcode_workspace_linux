764654d8a2e12176ce9060a0c61a8bcef666c292
Walber S Mota <mota.csharp@gmail.com>
2023-12-22 16:50:57 -0300

    updte pass.cpp pass.h(gtkmm)

--- gtkmm/mais-estudosGTK/pass.h
+++ gtkmm/mais-estudosGTK/pass.h
@@ -2,13 +2,15 @@
 #define PASS_H
 #include <gtkmm/window.h>
 #include <gtkmm/application.h>
+#include <gtkmm/box.h>
+
 
 class janela : public Gtk::Window
 {
 
 public:
     janela();
-    //~janela();
+    ~janela() override;
 };
 
 #endif
