7a46bf7fd9aa081e1709542568fd6fb60e4ccd02
Walber S Mota <mota.csharp@gmail.com>
2023-12-21 21:08:26 -0300

    update janela.cpp(gtkmm)

--- POO_Marcos Oliveira/gtkmm/janela.cpp
+++ POO_Marcos Oliveira/gtkmm/janela.cpp
@@ -14,7 +14,15 @@ janela1::janela1()
 
 int main(int argc, char* argv[])
 {
+
   auto app = Gtk::Application::create("org.gtkmm.examples.base");
+    /* cria um objeto Gtk::Application, armazenado em um smartpointer Glib::RefPtr. Isso é necessário em todos os aplicativos gtkmm. O método create() para este objeto inicializa o gtkmm. */
+
 
   return app->make_window_and_run<janela1>(argc, argv);
+/* Os argumentos argc e argv, passados ​​para seu aplicativo na linha de comando, podem ser verificados quando run() é chamado, mas este aplicativo simples não usa esses argumentos.
+A última linha mostra a janela e entra no loop de processamento principal do gtkmm, que terminará quando a janela for fechada.
+*/
+
+
 }
