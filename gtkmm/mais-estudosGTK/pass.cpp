22e66a71c7f3ba1dfa8bec992873df230be5c519
Walber S Mota <mota.csharp@gmail.com>
2023-12-22 17:10:27 -0300

    update pass.cpp(gtkmm)

--- gtkmm/mais-estudosGTK/pass.cpp
+++ gtkmm/mais-estudosGTK/pass.cpp
@@ -6,19 +6,15 @@ janela::janela()
     set_title("Gerenciador de Senha - em construção(22/12/2023)");
     set_default_size(500, 300);
     set_visible(true);
-    Gtk::Button m_botao1("Ola,usuário!");// cria o objeto botao com o rótulo "Ola,usuário!"
-  m_button.set_margin(10);
-
-
-
-
 
 
 
 }
 
 
-{
+Gtk::Button m_botao1("Ola,usuário!")// cria o objeto botao com o rótulo "Ola,usuário!"
+{    
+    m_botao1.set_margin(10);
   // Sets the margin around the button.
   m_button.set_margin(10);
 
