72bc1dc39929e07b84757a8b4b9cb5d57dd3558d
Walber S Mota <mota.csharp@gmail.com>
2023-12-23 23:05:41 -0300

    update pass.cpp(gtkmm)

--- gtkmm/mais-estudosGTK/pass.cpp
+++ gtkmm/mais-estudosGTK/pass.cpp
@@ -4,7 +4,7 @@
 janela::janela()
 {
     set_title("Gerenciador de Senha - em construção(22/12/2023)");
-    set_default_size(500, 300);
+    set_default_size(300, 300);
     set_visible(true);
     Gtk::Button m_botao1("Ola,usuário!");
 
