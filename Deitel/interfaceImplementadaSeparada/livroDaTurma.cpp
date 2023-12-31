c6a58eb3bae021145be53c220db7e9c2cf43ca04
WalberMota <mota.csharp@gmail.com>
2023-12-31 06:29:11 -0300

    update livroDaTurma.cpp

--- Deitel/interfaceImplementadaSeparada/livroDaTurma.cpp
+++ Deitel/interfaceImplementadaSeparada/livroDaTurma.cpp
@@ -1,3 +1,11 @@
+/* 
+cada nome de função-membro nos cabeçalhos de função (linhas 13, 18, 25 e 21) é precedido pelo nome de
+classe e por ::, que é conhecido como "operador de resolução de escopo binário". Isso ‘amarra’ cada
+função-membro à definição de classe livroDaTurma (agora separada em ), que declara as funções-membro
+ e membros de dados da classe. Sem "livroDaTurma::" precedendo cada nome de função, essas funções não
+ seriam reconhecidas pelo compilador como funções-membro da classe livroDaTurma.
+ */
+
 #include <iostream>
 #include "livroDaTurma.h"
 
@@ -19,7 +27,7 @@ livroDaTurma::livroDaTurma(string name)
         return courseName; // retorna courseName do objeto
     }                      // fim da função getCourseName
 
-    // exibe uma mensagem de boas-vindas para o usuário GradeBook
+    // exibe uma mensagem de boas-vindas para o usuário livroDaTurma
     void livroDaTurma::displayMessage()
     {
         // chama getCourseName para obter o courseName
