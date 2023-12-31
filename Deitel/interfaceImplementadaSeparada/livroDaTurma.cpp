000442862c254035fc677143baef2982b317beaa
WalberMota <mota.csharp@gmail.com>
2023-12-31 06:08:06 -0300

    update livroDaTurma.cpp

--- Deitel/interfaceImplementadaSeparada/livroDaTurma.cpp
+++ Deitel/interfaceImplementadaSeparada/livroDaTurma.cpp
@@ -1,25 +1,26 @@
+#include <iostream>
 #include "livroDaTurma.h"
 
 
-livroDaTurma(string name)
+livroDaTurma::livroDaTurma(string name)
     {
         setCourseName(name); // chama a função SET para inicializar courseName(nome do curso)
     }                        // fim do construtor livroDaTurma
 
     // função SETTER = configurar o nome do curso
-    void setCourseName(string name)
+    void livroDaTurma::setCourseName(string name)
     {
         courseName = name; // armazena o nome do curso no objeto curseName
     }                      // fim da função setCourseName
 
     // função GET
-    string getCourseName()
+    string livroDaTurma::getCourseName()
     {
         return courseName; // retorna courseName do objeto
     }                      // fim da função getCourseName
 
     // exibe uma mensagem de boas-vindas para o usuário GradeBook
-    void displayMessage()
+    void livroDaTurma::displayMessage()
     {
         // chama getCourseName para obter o courseName
         std::cout << "Bem-vindo ao livro de Classe do curso: " << getCourseName() << "!" << std::endl;
