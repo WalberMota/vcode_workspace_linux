7c6b6876bb1508d6fbcfb09ec57b289a3dbc70ed
WalberMota <mota.csharp@gmail.com>
2023-12-31 06:03:08 -0300

    update livroDaTurma.h

--- Deitel/interfaceImplementadaSeparada/livroDaTurma.h
+++ Deitel/interfaceImplementadaSeparada/livroDaTurma.h
@@ -8,31 +8,11 @@ using std::string;
 class livroDaTurma
 {
 public:
-    // o construtor da classe livroDaTurma
-    livroDaTurma(string name)
-    {
-        setCourseName(name); // chama a função SET para inicializar courseName(nome do curso)
-    }                        // fim do construtor livroDaTurma
-
-    // função SETTER = configurar o nome do curso
-    void setCourseName(string name)
-    {
-        courseName = name; // armazena o nome do curso no objeto curseName
-    }                      // fim da função setCourseName
-
-    // função GET
-    string getCourseName()
-    {
-        return courseName; // retorna courseName do objeto
-    }                      // fim da função getCourseName
-
-    // exibe uma mensagem de boas-vindas para o usuário GradeBook
-    void displayMessage()
-    {
-        // chama getCourseName para obter o courseName
-        std::cout << "Bem-vindo ao livro de Classe do curso: " << getCourseName() << "!" << std::endl;
-    } // fim da função displayMessage
-
+     livroDaTurma(string name);
+     void setCourseName(string name);
+     string getCourseName();
+     void displayMessage();
+ 
 private://a variável curseName está encapsulada, não poder ser acessada de fora dessa classe
     string courseName; // nome do curso para esse livroDaTurma
 
