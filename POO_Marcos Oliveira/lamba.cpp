27ab499dd6dd44fba328cda7ab57d30224cc1862
WalberMota <mota.csharp@gmail.com>
2024-01-03 20:15:39 -0300

    updated lamba.cpp

--- POO_Marcos Oliveira/lamba.cpp
+++ POO_Marcos Oliveira/lamba.cpp
@@ -2,10 +2,16 @@
 #include<algorithm> //find_if_not   
 #include<iterator> //std::begin e std::end
 
+bool eh_letra(char c){
 
+    return (c>='A'&& c<='Z') || (c>='a' && c<='z')
+}
 
 int main(int argc, char const *argv[])
 {
     const char str[] = "Hhsj5jsjd";
+    const char *it = std::find_if_not(std::begin(str),std.end(str),eh_letra);
+
+    std::cout << "o caracter que não é letra em str é :" << *it <<'\n';
     return 0;
 }
