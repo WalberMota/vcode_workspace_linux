809d54d60a9c40d532d2d3da158b36d1c28e1214
WalberMota <mota.csharp@gmail.com>
2024-01-23 22:25:38 -0300

    update getters e setters

--- getters_setters/exampleGettersSetters.cpp
+++ getters_setters/exampleGettersSetters.cpp
@@ -6,14 +6,14 @@ class Movie
 private:
     std::string nomeFilme;
     std::string diretorFilme;
-    std::string notaFilme;
+    std::string CalssificacaoFilme;
 
 public:
     Movie(std::string setNome, std::string setDiretor, std::string setNota)
     {
         nomeFilme = setNome;
         diretorFilme = setDiretor;
-        notaFilme = setNota;
+        CalssificacaoFilme = setNota;
         std::cout << "objeto criado\n";
     }
 
@@ -22,9 +22,28 @@ public:
 
 int main(int argc, char const *argv[])
 {
-    Movie filme1("E.T.: O Extraterrestre","Steven Spielberg","9.6");
-    Movie filme2("A Idade do Ouro","Luis Buñuel", "7.2");
+    Movie filme1("E.T.: O Extraterrestre","Steven Spielberg","PG");
+    Movie filme2("A Idade do Ouro","Luis Buñuel", "PG-13");
  
      
     return 0;
 }
+
+
+/* 
+G – General Audiences (Público em Geral)
+Todas as idades são permitidas. Nada que possa ofender os pais para ser visto por crianças.
+
+PG – Parental Guidance Suggested (Sugestão de Orientação Parental)
+Alguns materiais podem não ser adequados para crianças. Os pais são incentivados a dar "orientação parental". Pode conter algum material que os pais talvez não gostem para seus filhos pequenos.
+
+PG-13 – Parents Strongly Cautioned (Os Pais são Fortemente Advertidos)
+Alguns materiais podem ser inadequados para crianças menores de 13 anos. Recomenda-se que os pais sejam cautelosos. Alguns materiais podem ser inadequados para pré-adolescentes.
+
+R – Restricted (Restrito)
+Menores de 17 anos precisam ser acompanhados pelos pais ou por um responsável adulto. Contém algum material adulto. Recomenda-se que os pais se informem mais sobre o filme antes de levar seus filhos pequenos.
+
+NC-17 – Adults Only (Somente para Adultos)
+Ninguém com 17 anos ou menos foi admitido. Claramente adulto. Crianças não são admitidas.
+
+ */
