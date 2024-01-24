32939c7269ef6ce6b693e021b5591f42443f91d0
WalberMota <mota.csharp@gmail.com>
2024-01-23 21:44:33 -0300

    getters e setters

---
+++ getters_setters/exampleGettersSetters.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+
+class Movie
+{
+public:
+    string nomeFilme;
+    string diretorFilme;
+    string notaFilme;
+
+public:
+    Movie(string nomeFilme, string diretorFilme, string notaFilme);
+    ~Movie();
+};
+
+Movie(string nome, string diretor, string nota)
+{
+    nomeFilme = nome;
+    diretorFilme = diretor;
+
+
+}
+
+~Filmes()
+{
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
