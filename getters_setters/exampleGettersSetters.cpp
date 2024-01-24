be9296960732544470c77e0408e2a866c8c11ab7
WalberMota <mota.csharp@gmail.com>
2024-01-23 22:18:01 -0300

    update getters e setters

--- getters_setters/exampleGettersSetters.cpp
+++ getters_setters/exampleGettersSetters.cpp
@@ -9,12 +9,12 @@ private:
     std::string notaFilme;
 
 public:
-    Movie(std::string nome, std::string diretor, std::string nota)
+    Movie(std::string setNome, std::string setDiretor, std::string setNota)
     {
-        nomeFilme = nome;
-        diretorFilme = diretor;
-        notaFilme = nota;
-        std::cout << "objeto criado";
+        nomeFilme = setNome;
+        diretorFilme = setDiretor;
+        notaFilme = setNota;
+        std::cout << "objeto criado\n";
     }
 
     ~Movie(){};
@@ -24,6 +24,7 @@ int main(int argc, char const *argv[])
 {
     Movie filme1("E.T.: O Extraterrestre","Steven Spielberg","9.6");
     Movie filme2("A Idade do Ouro","Luis Buñuel", "7.2");
-
+ 
+     
     return 0;
 }
