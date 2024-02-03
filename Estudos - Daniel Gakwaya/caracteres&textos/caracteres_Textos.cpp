01dc5ddfc0d53590a3e4e17634bad0765bf671a4
WalberMota <mota.csharp@gmail.com>
2024-02-03 17:24:19 -0300

    D.Gakwaya caracteres_Textos.cpp

--- Estudos - Daniel Gakwaya/caracteres&textos/caracteres_Textos.cpp
+++ Estudos - Daniel Gakwaya/caracteres&textos/caracteres_Textos.cpp
@@ -9,18 +9,18 @@ int main(){
     char caracter4 {'o'};
     char caracter5 {'w'};
     
-    std::cout << caracter1 << std::endl;
-    std::cout << caracter2 << std::endl;
-    std::cout << caracter3 << std::endl;
-    std::cout << caracter4 << std::endl;
-    std::cout << caracter5 << std::endl;
+    std::cout << caracter1 << '\n';
+    std::cout << caracter2 << '\n';
+    std::cout << caracter3 << '\n';
+    std::cout << caracter4 << '\n';
+    std::cout << caracter5 << '\n';
 
     //Um byte na memoria = 2^8 = 256 diferentes valores (0~255)
-    std::cout << std::endl;
+    std::cout << '\n';
     
     char value = 65 ; // código ASCII para 'A'
-    std::cout << "value : " << value << std::endl; // A
-    std::cout << "value(int) : " << static_cast<int>(value) << std::endl; 
+    std::cout << "value : " << value << '\n'; // A
+    std::cout << "value(int) : " << static_cast<int>(value) << '\n'; 
 
     return 0;
 }
