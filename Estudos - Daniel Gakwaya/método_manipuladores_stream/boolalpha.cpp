b5b8596a48bb2b93077fc879e6215f1ad6107b6f
WalberMota <mota.csharp@gmail.com>
2024-02-11 12:32:24 -0300

    update boolalpha.cpp

--- Estudos - Daniel Gakwaya/método_manipuladores_stream/boolalpha.cpp
+++ Estudos - Daniel Gakwaya/método_manipuladores_stream/boolalpha.cpp
@@ -10,19 +10,19 @@ int main(int argc, char const *argv[])
     bool condition{true};
     bool other_condition{false};
 
-    std::cout <<"Condição: " << condition <<'\n';
-    std::cout <<"Outra condição: " << other_condition <<"\n\n";
+    std::cout <<"Condição verdadeira: " << condition <<'\n';
+    std::cout <<"Condição falsa: " << other_condition <<"\n\n";
 
     
     std::cout<<std::boolalpha;
     std::cout<<"Usando std::boolalpha obtemos a pala true e false ao invés de 1 e 0\n";
-    std::cout <<"Condição: " << condition <<'\n';
-    std::cout <<"Outra condição: " << other_condition <<"\n\n";
+    std::cout <<"Condição verdadeira: " << condition <<'\n';
+    std::cout <<"Condição falsa: " << other_condition <<"\n\n";
 
     std::cout<<std::noboolalpha;
     std::cout<<"Usando std::noboolalpha obtemos novamente 1 e 0 (default)\n";
-    std::cout <<"Condição: " << condition <<'\n';
-    std::cout <<"Outra condição: " << other_condition <<'\n';
+    std::cout <<"Condição verdadeira: " << condition <<'\n';
+    std::cout <<"Condição falsa: " << other_condition <<'\n';
 
 
     return 0;
