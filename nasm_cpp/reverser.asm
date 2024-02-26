33a2f8975b6c07f2f4bf7d217d63f37721b90929
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-02-26 17:41:36 -0300

    update reverser.asm

--- nasm_cpp/reverser.asm
+++ nasm_cpp/reverser.asm
@@ -5,36 +5,37 @@ section. text
 
 
 _start:
-		push ebp
-		mov ebp,esp
-		push esi
-		push edi
+	push ebp
+	mov ebp,esp
+	push esi
+	push edi
 
+	xor	eax,eax
+	mov	edi,[ebp+8]
+	mov	esi,[ebp+12]
+	mov ecx,[ebp+16]
+	test	ecx,ecx
 
-		xor	eax,eax
-		mov	edi,[ebp+8]
-		mov	esi,[ebp+12]
-		mov ecx,[ebp+16]
-		test	ecx,ecx
-
-		lea esi,[esi+ecx*4-4]
-		pushfd
-		std
+	lea esi,[esi+ecx*4-4]
+	pushfd
+	std
 
 @@:		
-        lodsd
-		mov	[edi],eax
-		add edi,4
-		dec ecx
-		jnz	@B
+    lodsd
+	mov	[edi],eax
+	add edi,4
+	dec ecx
+	jnz	@B
 
-		popfd
-		mov eax,1
+	popfd
+	mov eax,1
 
-		pop edi
-		pop esi
-		pop ebp
+	pop edi
+	pop esi
+	pop ebp
 
-		ret
-
-        
+	ret
+saida:
+    mov     eax,0x1         ;indica o final de operação, corresponde a System.exit
+    mov     ebx,0x0         ;informa o estado final do programa - 0 sem erro
+    int 0x80                 
