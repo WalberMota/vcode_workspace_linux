e860bf95125bce34ac0942c1525e9843f60bfdcc
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-03-08 19:23:36 -0300

    update fahrenheit.cpp

--- Fisica/fahrenheit.cpp
+++ Fisica/fahrenheit.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
+#include <iomanip>
 
 int main(int argc, char const *argv[])
 {
-    int t_menor,t_maior, incremento;
-    float fahr, celsius;
+    std::cout.unsetf(std::ios::scientific | std::ios::fixed);
+    double t_menor,t_maior, incremento;
+    double fahr, celsius;
+    std::setprecision(1);
+    t_menor=10.2;
+    t_maior=100;
+    incremento=10;
 
-    t_menor=0;
-    t_maior=300;
-    incremento=20;
+    celsius=t_menor;
 
-    fahr=t_menor;
-
-    std::cout << "\n|   Fahrenheit\t|     Celsius\t|\n";
+    std::cout << "\n|   Celsius\t|     Fahrenheit\t|\n";
     std::cout << "+-------------------------------+\n";
-    while (fahr <= t_maior)
+    
+    while (celsius <= t_maior)
     {
-        celsius=(5.0/9.0) * (fahr-32.0);
-        std::cout <<"|\tfahr\t|\tcelsius\t|\n";
+        fahr=(9 * celsius + 160) / 5;
+        std::cout <<"|\t"<<std::setprecision(2)<<celsius<<"\t|\t"<< std::fixed<<fahr<<"\t|\n";
         printf ("+-------------------------------+\n");        
-        fahr = fahr+incremento;
+        celsius = celsius+incremento;
     }
     
 
