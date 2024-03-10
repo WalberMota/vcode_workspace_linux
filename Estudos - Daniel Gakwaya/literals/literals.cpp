3086a3a645ba6f4c3525e17b52af0749c388281a
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-03-09 21:25:02 -0300

    estudo de literais D.Gakwaya

--- Estudos - Daniel Gakwaya/literals/literals.cpp
+++ Estudos - Daniel Gakwaya/literals/literals.cpp
@@ -8,13 +8,14 @@ int main(int argc, char const *argv[])
     std::cout << "O preço é : " << prize << std::endl;
     std::cout << "signed_long_long_int : " << signed_long_long_int << std::endl;
 
-    //Com sistemas numéricos - Hex: prefixo com 0x
-    unsigned int hex_number1{ 0x22BU}; // Decimal 555
-    int hex_number2 {0x400};// Decimal 1024
-    std::cout << std::hex <<  "\nO numero hexadecimal_1 é : " << hex_number1 << std::endl;
-    std::cout << std::dec <<  "O numero hexadecimal_2 é : " << hex_number2 << std::endl;
+    // Com sistemas numéricos - Hex: prefixo com 0x
+    unsigned int hex_number1{0x22BU}; // Decimal 555
+    int hex_number2{0x400};           // Decimal 1024
+    std::cout << std::hex << "\nO numero hexadecimal_1 é : " << hex_number1 << std::endl;
+    std::cout << std::dec << "O numero hexadecimal_2 é : " << hex_number2 << std::endl;
 
-
+    unsigned int binary_literal{0b11111111u}; // 255 dec
+    std::cout << "A literal binária é : " << binary_literal << std::endl;
 
     return 0;
 }
