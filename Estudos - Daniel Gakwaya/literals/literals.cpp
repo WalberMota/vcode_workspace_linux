883b96ab22189cca327e63f60c10444765ac36e7
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-03-09 21:17:39 -0300

    estudo de literais D.Gakwaya

--- Estudos - Daniel Gakwaya/literals/literals.cpp
+++ Estudos - Daniel Gakwaya/literals/literals.cpp
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[])
 {
     signed long long int signed_long_long_int{1234ll};
     unsigned int prize{1'500'00'0u};
-    std::cout << "The prize is : " << prize << std::endl;
+    std::cout << "O preço é : " << prize << std::endl;
 
     std::cout << "signed_long_long_int : " << signed_long_long_int << std::endl;
 
