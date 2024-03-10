c7cc48416511a9f78d58c9f8ca747ded61d48fc7
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-03-09 21:20:11 -0300

    estudo de literais D.Gakwaya

--- Estudos - Daniel Gakwaya/literals/literals.cpp
+++ Estudos - Daniel Gakwaya/literals/literals.cpp
@@ -6,8 +6,15 @@ int main(int argc, char const *argv[])
     signed long long int signed_long_long_int{1234ll};
     unsigned int prize{1'500'00'0u};
     std::cout << "O preço é : " << prize << std::endl;
-
     std::cout << "signed_long_long_int : " << signed_long_long_int << std::endl;
+
+    //Com sistemas numéricos - Hex: prefixo com 0x
+    unsigned int hex_number1{ 0x22BU}; // Dec 555
+    int hex_number2 {0x400};// Dec 1024
+    std::cout << std::hex <<  "O numero hexadecimal_1 é : " << hex_number << std::endl;
+    std::cout << std::dec <<  "O numero hexadecimal_2 é : " << hex_number2 << std::endl;
+
+
 
     return 0;
 }
