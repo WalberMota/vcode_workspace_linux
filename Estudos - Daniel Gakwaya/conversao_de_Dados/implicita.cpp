0969d7df2989a7011162b8d912650e0d6ed781bd
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-03-25 19:09:34 -0300

    update conversão implicita - DGakwaya

--- Estudos - Daniel Gakwaya/conversao_de_Dados/implicita.cpp
+++ Estudos - Daniel Gakwaya/conversao_de_Dados/implicita.cpp
@@ -1,10 +1,10 @@
 /*
-O compilador aplica conversões implícitas
-quando os tipos são diferentes em uma expressão
+    O compilador aplica conversões implícitas
+    quando os tipos são diferentes em uma expressão
 
-As conversões são sempre feitas a partir do menor para
-o maior tipo neste caso int é transformado em double
-antes da expressão é avaliado. A menos que estejamos fazendo uma tarefa
+    As conversões são sempre feitas a partir do menor para
+    o maior tipo neste caso int é transformado em double
+    antes da expressão é avaliado. A menos que estejamos fazendo uma tarefa
 */
 
 #include <iostream>
@@ -25,8 +25,8 @@ int main(int argc, char const *argv[])
 
 
 /*
-a operação de atribuição(feita pelo usuário) causará uma conversão estreita implícita.
-y é convertido em int antes da atribuição
+    a operação de atribuição(feita pelo usuário) causará uma conversão estreita implícita.
+    y é convertido em int antes da atribuição
 */
 
     int x;
