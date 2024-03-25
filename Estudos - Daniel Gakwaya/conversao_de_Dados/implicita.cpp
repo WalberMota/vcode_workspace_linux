71b0529955f2039119268d9cc65bcd7a83a163df
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-03-25 18:38:42 -0300

    conversão implicita - DGakwaya

---
+++ Estudos - Daniel Gakwaya/conversao_de_Dados/implicita.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+
+    double preco{45.6};
+    int unidades{10};
+    double total = preco * unidades;
+
+    std::cout << "o preço por unidade é :" << preco << '\n';
+    std::cout << "unidade adquidas      :" << unidades << '\n';
+    std::cout << "Total da aquisição     :" << total << '\n';
+
+    return 0;
+}
