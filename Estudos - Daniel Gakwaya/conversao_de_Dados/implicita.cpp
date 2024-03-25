a0db044394ca72a42bb595f1ef1a9b16e293f7c4
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-03-25 18:40:57 -0300

    update conversão implicita - DGakwaya

--- Estudos - Daniel Gakwaya/conversao_de_Dados/implicita.cpp
+++ Estudos - Daniel Gakwaya/conversao_de_Dados/implicita.cpp
@@ -9,7 +9,8 @@ int main(int argc, char const *argv[])
 
     std::cout << "o preço por unidade é :" << preco << '\n';
     std::cout << "unidade adquidas      :" << unidades << "\n\n";
-    std::cout << "Total da aquisição     :" << total << '\n';
+    std::cout << "Total da aquisição     :" << total << '\n'
+              << std::endl;
 
     return 0;
 }
