a28f83f46b2b80cde80d5ff0c0193bbbcd655634
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-04-11 19:51:04 -0300

    update bitwisecomposto.cpp

--- Estudos - Daniel Gakwaya/OperadoresCompostosBitwise/bitwisecompostos.cpp
+++ Estudos - Daniel Gakwaya/OperadoresCompostosBitwise/bitwisecompostos.cpp
@@ -15,10 +15,18 @@ int main(int argc, char const *argv[])
     std::cout << std::endl;
     std::cout << "valor inicial: " << std::endl;
 
-    std::cout << std::setw(lagura_coluna) <<"binario inicial" <<
+    std::cout << std::setw(lagura_coluna) <<"binario :" <<
     std::setw(lagura_coluna) << std::bitset<8>(binario) << std::endl;
     std::cout << std::endl;
-    
+
+    //realizando deslocamento à esquerda (left shift)
+    std::cout << std::endl;
+    std::cout << "Deslocar para a esquerda 2 posições de bits no lugar: " << std::endl;
+    binario <<=2;
+    std::cout << std::setw(lagura_coluna) <<"binario :" <<
+    std::setw(lagura_coluna) << std::bitset<8>(binario) << std::endl;
+    std::cout << std::endl;
+
 
     return 0;
 }
