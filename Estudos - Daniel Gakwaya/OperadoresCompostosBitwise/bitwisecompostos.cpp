4b1f18236ab1c014fd6531af6d89d45b6d8f9efe
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-04-11 19:45:04 -0300

    update bitwisecomposto.cpp

--- Estudos - Daniel Gakwaya/OperadoresCompostosBitwise/bitwisecompostos.cpp
+++ Estudos - Daniel Gakwaya/OperadoresCompostosBitwise/bitwisecompostos.cpp
@@ -2,8 +2,11 @@
 #include <iomanip>
 #include <bitset>
 
+const int lagura_coluna {20};
+
 int main(int argc, char const *argv[])
 {
+
     std::cout << "Operação com bitwise Compostos" << "/n";
 
     unsigned char binario{0b00110100};
@@ -12,6 +15,7 @@ int main(int argc, char const *argv[])
     std::cout << std::endl;
     std::cout << "valor inicial: " << std::endl;
 
-
+    std::cout << std::setw(lagura_coluna) <<"binario inicial" <<
+    std::setw(lagura_coluna) << std::bitset<8>(binario);
     return 0;
 }
