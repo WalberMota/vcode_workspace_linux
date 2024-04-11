6c04c1cdab3afd2f1c9859d4621f1e3554a6d140
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-04-11 19:39:34 -0300

    update bitwisecomposto.cpp

--- Estudos - Daniel Gakwaya/OperadoresCompostosBitwise/bitwisecompostos.cpp
+++ Estudos - Daniel Gakwaya/OperadoresCompostosBitwise/bitwisecompostos.cpp
@@ -1,15 +1,13 @@
-#include<iostream>
-#include<iomanip>
-#include<bitset>
+#include <iostream>
+#include <iomanip>
+#include <bitset>
 
 int main(int argc, char const *argv[])
 {
-std::cout <<"Operação com bitwise Compostos" << "/n";
+    std::cout << "Operação com bitwise Compostos"
+              << "/n";
 
-
-
+    unsigned char binario{0b00110100};
 
     return 0;
 }
-
-
