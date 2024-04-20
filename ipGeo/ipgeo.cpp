2a9edc0eaf084b53407e30c0821424efb627a3a3
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-04-19 21:44:43 -0300

    updated ipgeo.cpp

--- ipGeo/ipgeo.cpp
+++ ipGeo/ipgeo.cpp
@@ -3,10 +3,18 @@
 
 int main(int argc, char const *argv[])
 {
-    std::string ip;
+    std::string ip, var1;
+    
     std::cout << "Informa a localização geográfica de um IP";
 
     std::cout <<"informe o IP:";
     std::cin >> ip;
+    
+    var1="curl https://ipinfo.io/"+ip;
+
+    std::cout << var1;
+    
+    //system (var1);
+
     return 0;
 }
