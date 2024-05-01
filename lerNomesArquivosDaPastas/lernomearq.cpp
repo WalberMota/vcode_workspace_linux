32a88050477ff2d7e4637b501e311d2823ce74b6
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-05-01 16:46:48 -0300

    updated lernomearq.cpp

--- lerNomesArquivosDaPastas/lernomearq.cpp
+++ lerNomesArquivosDaPastas/lernomearq.cpp
@@ -3,6 +3,8 @@
 #include <string>
 
 int main() {
+    std::string priparte="ffmpeg -i ";
+    std::string restopart=" -vn -ar 44100 -ac 2 -ab 96k -f mp3";
     // Define o caminho da pasta que você deseja ler
     std::filesystem::path dir("/home/preacher/Music/meus_etheralT/teste");
 
@@ -14,6 +16,9 @@ int main() {
             std::string fileName = entry.path().filename().string();
             //fileName.erase(std::remove_if(fileName.begin(), fileName.end(), ::ispunct), fileName.end());
             std::cout << fileName << std::endl;
+
+            system('ffmpeg -i <arquivo_fonte> -vn -ar 44100 -ac 2 -ab 96k -f mp3 <arquivo.saida>.mp3');
+
         }
     }
 
