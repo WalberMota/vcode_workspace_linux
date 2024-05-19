c3762b155a20531087a0f4a57d14389cf919c1da
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-05-19 15:38:47 -0300

    update ifcominicializador.cpp

--- Estudos - Daniel Gakwaya/ifComInicializador/ifcominicializador.cpp
+++ Estudos - Daniel Gakwaya/ifComInicializador/ifcominicializador.cpp
@@ -2,6 +2,18 @@
 
 int main(int argc, char const *argv[])
 {
-    
+    int speed {10};
+    bool go {false};
+
+    if (go) {
+
+        if (speed < 5){
+            
+        }
+
+
+    }
+
+
     return 0;
 }
