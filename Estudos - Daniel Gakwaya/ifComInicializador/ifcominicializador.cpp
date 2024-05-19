7547c41d238a40e25c262a492ea5d2828bd641b1
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-05-19 16:03:37 -0300

    update ifcominicializador.cpp

--- Estudos - Daniel Gakwaya/ifComInicializador/ifcominicializador.cpp
+++ Estudos - Daniel Gakwaya/ifComInicializador/ifcominicializador.cpp
@@ -5,8 +5,7 @@ int main(int argc, char const *argv[])
     
     bool go {true};
 
-    if (int speed {50};go) {
-
+    if (int speed {51};go) {//com go{true} executa este teste 
         std::cout << "Velocidade: " << speed << std::endl;
 
 
