22411a413472dfb66b7b908a17170301efc56b57
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-05-27 19:28:02 -0300

    Daniel Gakwaya-udemy-tarefa6

--- Estudos - Daniel Gakwaya/Udemy_exercicios/Tarefa6/main.cpp
+++ Estudos - Daniel Gakwaya/Udemy_exercicios/Tarefa6/main.cpp
@@ -1,34 +1,21 @@
   #include<iostream> 
 
-  int diaDaSemana;
-   
- int main() {  
-   system("clear");
+int diaDaSemana;
+void criamenu();
 
-    std::cout << "#################################\n";
-    std::cout << "#      Digite um numero         #\n";
-    std::cout << "#         para o dia            #\n";
-    std::cout << "#    da semana desejado:        #\n";
-    std::cout << "#                               #\n";
-    std::cout << "#    1 - Segunda - feira        #\n";
-    std::cout << "#    2 - Terça - Feira          #\n";
-    std::cout << "#    3 - Quarta - Feira         #\n";
-    std::cout << "#    4 - Quinta - Feira         #\n";
-    std::cout << "#    5 - Sexta - Feira          #\n";
-    std::cout << "#    6 - Sábado                 #\n";
-    std::cout << "#    7 - Domingo                #\n";
-    std::cout << "#    0 - Terminar               #\n";
-    std::cout << "#                               #\n";
-    std::cout << "#################################\n";
-    std::cout << "\nDigite o numero do dia da semana desejado : ";
-
+int main() {
+    criamenu();
     do
     {
+  
+    std::cout << "\nDigite o numero do dia da semana desejado : ";
+
+
         std::cin >> diaDaSemana;
         switch (diaDaSemana)
         {
         case 1:
-
+            system("clear");
             std::cout << "\nO dia escolhido foi Segunda-Feira\n";
             break;
         case 2:
@@ -59,4 +46,21 @@
         return 0;
 
  }
-    
+void criamenu()
+{
+std::cout << "#################################\n";
+    std::cout << "#      Digite um numero         #\n";
+    std::cout << "#         para o dia            #\n";
+    std::cout << "#    da semana desejado:        #\n";
+    std::cout << "#                               #\n";
+    std::cout << "#    1 - Segunda - feira        #\n";
+    std::cout << "#    2 - Terça - Feira          #\n";
+    std::cout << "#    3 - Quarta - Feira         #\n";
+    std::cout << "#    4 - Quinta - Feira         #\n";
+    std::cout << "#    5 - Sexta - Feira          #\n";
+    std::cout << "#    6 - Sábado                 #\n";
+    std::cout << "#    7 - Domingo                #\n";
+    std::cout << "#    0 - Terminar               #\n";
+    std::cout << "#                               #\n";
+    std::cout << "#################################\n";
+}
