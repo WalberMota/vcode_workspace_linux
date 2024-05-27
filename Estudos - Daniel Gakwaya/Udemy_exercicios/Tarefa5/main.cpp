43ece55d9aa61f8a193a0e10e9d08f6c9c0c4e79
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-05-27 18:36:10 -0300

    Daniel Gakwaya-udemy-tarefa5

--- Estudos - Daniel Gakwaya/Udemy_exercicios/Tarefa5/main.cpp
+++ Estudos - Daniel Gakwaya/Udemy_exercicios/Tarefa5/main.cpp
@@ -1,10 +1,24 @@
 #include<iostream>
 
 void menu();
-
+std::string idade;
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    menu();
     return 0;
+}
+
+void menu()
+{
+    std::cout << "###############################\n";
+    std::cout << "#      Digite a sua idade      #\n";
+    std::cout << "#                              #\n";
+    std::cout << "#    São elegíveis as idades   #\n";
+    std::cout << "# entre 21 e 39 anos(inclusive)#\n";
+    std::cout << "#                              #\n";
+    std::cout << "###############################\n";
+    std::cout << "\nDigite a sua idade : ";
+    getline (std::cin,idade);
+
 }
