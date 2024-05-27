e937afa6b4113c15334358489479c09763bf61a4
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-05-27 19:21:48 -0300

    Daniel Gakwaya-udemy-tarefa6

--- Estudos - Daniel Gakwaya/Udemy_exercicios/Tarefa6/main.cpp
+++ Estudos - Daniel Gakwaya/Udemy_exercicios/Tarefa6/main.cpp
@@ -17,6 +17,7 @@
     std::cout << "#    5 - Sexta - Feira          #\n";
     std::cout << "#    6 - Sábado                 #\n";
     std::cout << "#    7 - Domingo                #\n";
+    std::cout << "#    0 - Terminar               #\n";
     std::cout << "#                               #\n";
     std::cout << "#################################\n";
     std::cout << "\nDigite o numero do dia da semana desejado : ";
@@ -27,20 +28,33 @@
         switch (diaDaSemana)
         {
         case 1:
-            // faz algo
+
+            std::cout << "\nO dia escolhido foi Segunda-Feira\n";
             break;
         case 2:
-            // faz algo
+            std::cout << "\nO dia escolhido foi Terça-Feira\n";
             break;
         case 3:
-            // faz algo
+            std::cout << "\nO dia escolhido foi Quarta-Feira\n";
             break;
         case 4:
+            std::cout << "\nO dia escolhido foi Quinta-Feira\n";
+            break;
+        case 5:
+            std::cout << "\nO dia escolhido foi Sexta-Feira\n";
+            break;
+        case 6:
+            std::cout << "\nO dia escolhido foi Sábado\n";
+            break;
+        case 7:
+            std::cout << "\nO dia escolhido foi Domingo\n";
+            break;
+        case 0:
             exit(0);
         
         }
     }
-        while (diaDaSemana != 7);
+        while (diaDaSemana != 0);
 
         return 0;
 
