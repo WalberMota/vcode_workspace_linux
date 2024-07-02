915fa8547b54bddda04c584d8f0bf678f8cdac0f
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-07-01 22:14:56 -0300

    comma operator

---
+++ Estudos - Daniel Gakwaya/CommaOperator/commaoperator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    int incremento{5};
+    int numero1{10};
+    int numero2{20};
+    int numero3{25};
+
+    int resultado = (numero1 *= ++incremento, numero2 - (++incremento), numero3 += ++incremento);
+
+    std::cout << numero1 << '\n';
+    std::cout << numero2 << '\n';
+    std::cout << numero3 << '\n';
+    std::cout << resultado << '\n';
+    return 0;
+}
