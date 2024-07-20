8636a2e34795cf372d01116376b683286cde9288
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-07-19 22:59:59 -0300

    update loop_baseado_em_intervalor.cpp

--- Estudos - Daniel Gakwaya/Loops/loop_baseado_em_intervalos.cpp
+++ Estudos - Daniel Gakwaya/Loops/loop_baseado_em_intervalos.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char const *argv[])
 {
-/*  //usando uma array
-
+    
+/*  
+    //usando uma array
     int bolsa_de_valores[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     for (int valor : bolsa_de_valores)
@@ -12,12 +14,18 @@ int main(int argc, char const *argv[])
         std::cout << "Valor na bolsa: " << valor << std::endl;
     }
 */
-
-    //especificando acoleção dentro do for
+/*
+    //especificando a coleção dentro do for
     for (int valor :{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}){
         std::cout << "Valor na bolsa: " << valor << std::endl;
     }
+*/
 
+    //compilador defino automaticamente o tipo de acordo com a coleção.
+    //o iterator é executado independente da ordem dos elementos
+    for (auto valor :{1.4, 5.66, 18.34, 4.04, 99.38, 125.4356}){
+        std::cout << "Valor na bolsa: " << valor << std::endl;
+    }    
 
     return 0;
 }
