a46155b520533210b6405d5abca71bf2d03449cc
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-07-19 22:40:07 -0300

    rename

---
+++ Estudos - Daniel Gakwaya/Loops/loop_baseado_em_intervalos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    int bolsa_de_valores[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    for (int valor : bolsa_de_valores)
+    {
+
+        std::cout << "Valor na bolsa: " << valor << std::endl;
+    }
+
+    return 0;
+}
