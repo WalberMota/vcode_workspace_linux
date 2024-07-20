434cc2413bd122f5cfb9c25a60727e0f49c2b91b
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-07-19 22:46:25 -0300

    update loop_baseado_em_intervalor.cpp

--- Estudos - Daniel Gakwaya/Loops/loop_baseado_em_intervalos.cpp
+++ Estudos - Daniel Gakwaya/Loops/loop_baseado_em_intervalos.cpp
@@ -4,11 +4,19 @@ int main(int argc, char const *argv[])
 {
     int bolsa_de_valores[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+ /*
     for (int valor : bolsa_de_valores)
     {
 
         std::cout << "Valor na bolsa: " << valor << std::endl;
     }
+*/
+
+    //especificando acoleção dentro do for
+    for (int valor :{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}){
+        std::cout << "Valor na bolsa: " << valor << std::endl;
+    }
+
 
     return 0;
 }
