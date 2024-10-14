0831e8bbd5e3a724fb870fedef26cf84e405ae24
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-10-14 20:37:45 -0300

    update Dum_v0.cpp

--- DUM/Dum_v0.cpp
+++ DUM/Dum_v0.cpp
@@ -1,22 +1,23 @@
 /*usando strftime*/
 # include <iostream>
-# include <time.h>
+# include <ctime>
+# include <string>
+
 
 
 int main(int argc, char const *argv[])
 {
-    time_t tempo;
+    std::string a;
 
-    struct tm *informatempo;
+    time_t hora;
+    struct tm *infotempo;
+    char memoria[10];
+    time (&hora);
+    infotempo = localtime (&hora);
 
-    char memoria[80];
-
-    time (&tempo);
-
-    informatempo = localtime (&tempo);
-
-    strftime (memoria,80,"Agora são %M:%p",informatempo);
-    
+    strftime (memoria,10,"%H:%M:%S",infotempo);
+    //puts(memoria);
+    std::cout << memoria << std::endl;//memoria;
 
 
     return 0;
