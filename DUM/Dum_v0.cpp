74fbd6f58be9a3f3225c1732e6b97acbef2ac3b7
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-10-14 20:51:31 -0300

    update Dum_v0.cpp

--- DUM/Dum_v0.cpp
+++ DUM/Dum_v0.cpp
@@ -1,24 +1,26 @@
 /*usando strftime*/
-# include <iostream>
-# include <ctime>
-# include <string>
-
-
+#include <iostream>
+#include <ctime>
+#include <string>
 
 int main(int argc, char const *argv[])
 {
-    std::string a;
+    //std::string a;
 
     time_t hora;
     tm *infotempo;
     char exibir[10];
-    time (&hora);
-    infotempo = localtime (&hora);
+    time(&hora);
+    infotempo = localtime(&hora);
 
-    strftime (exibir,10,"%H:%M:%S",infotempo);
-    //puts(exibir);
-    std::cout << exibir << std::endl;//exibir;
-
+    strftime(exibir, 10, "%H:%M:%S", infotempo);
+    // puts(exibir);
+    std::cout << exibir << std::endl; // exibir;
+    std::cout << "\nDIA: " << infotempo->tm_mday << '\n';
+    int dia= infotempo->tm_mday;
+    std::cout << dia+10 << '\n';
+    std::cout << "MES: " << 1 + infotempo->tm_mon;
+    std::cout << "ANO" << 1900 + infotempo->tm_year << '\n';
 
     return 0;
 }
