15b5ea47bb2d3edb15abeff2286d1bebc32c8a03
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-10-15 19:34:58 -0300

    tabela_IG_MS2014.md

--- DUM/DiferencaEmDias.cpp
+++ DUM/DiferencaEmDias.cpp
@@ -7,12 +7,12 @@ int main()
     struct tm data2 = {0}; // data final
 
     // preenche as datas
-    data1.tm_year = 2022 - 1900; // ano - 1900
-    data1.tm_mon = 0;            // mês (0-11)
-    data1.tm_mday = 1;           // dia
-    data2.tm_year = 2022 - 1900;
-    data2.tm_mon = 0;
-    data2.tm_mday = 10;
+    data1.tm_year = 2023 - 1900; // ano - 1900
+    data1.tm_mon = 11;            // mês (0-11)
+    data1.tm_mday = 12;           // dia
+    data2.tm_year = 2024 - 1900;
+    data2.tm_mon = 2;
+    data2.tm_mday = 5;
 
     // converte as datas para segundos
     time_t segundos1 = mktime(&data1);
@@ -21,10 +21,9 @@ int main()
     // calcula a diferença em segundos
     double diferenca = difftime(segundos2, segundos1);
 
-    // converte a diferença em dias
+    // converte a diferença em dias e imprime
     int dias = diferenca / 86400;
-
-    std::cout << "A diferença entre as datas é de " << dias << " dias.\n";
+    std::cout << "A diferença entre as datas é de " << (dias/7) << " semanas.\n";
 
     return 0;
 }
---
+++ DUM/tabela do MS2014.md
@@ -0,0 +1,10 @@
+Trimestre		Meses	Semanas
+1º trimestre	1 mês	até 4 semanas e meia de gestação
+1º trimestre	2 meses	4 semanas e meia a 9 semanas
+1º trimestre	3 meses	10 a 13 semanas e meia de gestação
+2º trimestre	4 meses	13 semanas e meia de gestação a 18 semanas
+2º trimestre	5 meses	19 a 22 semanas e meia de gestação
+2º trimestre	6 meses	23 a 27 semanas de gestação
+3º trimestre	7 meses	28 a 31 semanas e meia de gestação
+3º trimestre	8 meses	32 a 36 semanas de gestação
+3º trimestre	9 meses	37 a 42 semanas de gestação
