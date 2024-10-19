55f2043222f95b02aaacf2b3175a3fa9fe21607e
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-10-19 17:28:33 -0300

    Update DiferencaEmDias.cpp

--- DUM/DiferencaEmDias.cpp
+++ DUM/DiferencaEmDias.cpp
@@ -2,60 +2,64 @@
 #include <ctime>
 #include <string>
 
-
-    int i;
+int i;
 int main()
 {
+    time_t hora;
+    tm *infotempo;
+    char exibir[10];
+    time(&hora);
+    infotempo = localtime(&hora);
+
     struct tm data1 = {0}; // data inicial
     struct tm data2 = {0}; // data final
 
-    //Recebe a DUM
-    int dumdia,dummes,dumano;
+    // Recebe a DUM
+    int dumdia, dummes, dumano;
     std::string dum, stringtempo;
 
-    //system("clear");
-    std::cout << "Digite a data da ultima menstruação somente numeros:\n";
-    //std::cin.ignore();
+    system("clear");
+    std::cout << "Digite a data da ultima menstruação somente numeros:ex. 02052024\n";
+    // std::cin.ignore();
     std::getline(std::cin, dum);
-    std::cout <<  dum.length()<<" caracteres "<< "\n\n";
-
-    for (i=0;i<2;i++){
-        
-            stringtempo=stringtempo+dum[i];
-            dumdia=std::stoi(stringtempo);
-                    
+    // std::cout <<  dum.length()<<" caracteres "<< "\n\n";
+    system("clear");
+    for (i = 0; i < 2; i++)
+    {
+        stringtempo = stringtempo + dum[i];
+        dumdia = std::stoi(stringtempo);
     }
-    stringtempo="";
-    for (i=2;i<4;i++){
-        
-            stringtempo=stringtempo+dum[i];
-            dummes=std::stoi(stringtempo);
-            
-        
+    stringtempo = "";
+    for (i = 2; i < 4; i++)
+    {
+        stringtempo = stringtempo + dum[i];
+        dummes = std::stoi(stringtempo);
     }
-    stringtempo="";
-    for (i=4;i<dum.length();i++){
-            stringtempo=stringtempo+dum[i];
-            dumano=std::stoi(stringtempo);
-            
-        }        
-    
+    stringtempo = "";
+    for (i = 4; i < dum.length(); i++)
+    {
+        stringtempo = stringtempo + dum[i];
+        dumano = std::stoi(stringtempo);
+    }
 
+    std::cout <<"Data da Última Menstruação = "<< dumdia << "/" << dummes << "/" << dumano << '\n';
 
-    std::cout << dumdia <<"/"<<dummes<<"/"<<dumano<<'\n';
-
-    
-    //std::cout << "dia da dum= "<< dumdia << '\n';
-    
+    // std::cout << "dia da dum= "<< dumdia << '\n';
 
     // preenche as datas
     data1.tm_year = dumano - 1900; // ano - 1900
-    data1.tm_mon = dummes;            // mês (0-11)
-    data1.tm_mday = dumdia;           // dia
-    data2.tm_year = 2024 - 1900;
-    data2.tm_mon = 1;
-    data2.tm_mday = 5;
+    data1.tm_mon = dummes;         // mês (0-11)
+    data1.tm_mday = dumdia;        // dia
 
+    // adquire a data atual
+    int ano = 1900 + infotempo->tm_year;
+    data2.tm_year = ano - 1900;
+
+    int mes = 1 + infotempo->tm_mon;
+    data2.tm_mon = mes;
+
+    int dia = infotempo->tm_mday;
+    data2.tm_mday = dia;
 
     // converte as datas para segundos
     time_t segundos1 = mktime(&data1);
@@ -65,18 +69,14 @@ int main()
     double diferenca = difftime(segundos2, segundos1);
 
     // converte a diferença em dias e imprime
-
-    
     int dias = diferenca / 86400;
-    
-    double restosemana=((diferenca/86400)/7) - (dias/7);
-    double restoemdias=restosemana*7;
+    //separa a fração decimal das semanas
+    double restosemana = ((diferenca / 86400) / 7) - (dias / 7);
+    double restoemdias = restosemana * 7;
 
+    //mostra o resultado
     std::cout << "Dias de gestação: " << dias << " dias.\n";
-    std::cout << "Semanas de gestação " << (dias/7) << " semanas e "<<restoemdias <<" dias."<<std::endl;
-    
-    //std::cout << "fração de semana " << restosemana << " semanas.\n";
-    //std::cout << "fração de dias " << restoemdias << " dias.\n";
-    
+    std::cout << "Semanas de gestação " << (dias / 7) << " semanas e " << restoemdias << " dias." << std::endl;
+
     return 0;
 }
