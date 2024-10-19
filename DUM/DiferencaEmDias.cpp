f64b09b79133b9e5181a6b13461ff110c32d6c05
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-10-19 16:47:19 -0300

    Update DiferencaEmDias.cpp

--- DUM/DiferencaEmDias.cpp
+++ DUM/DiferencaEmDias.cpp
@@ -13,42 +13,49 @@ int main()
     int dumdia,dummes,dumano;
     std::string dum, stringtempo;
 
-    system("clear");
+    //system("clear");
     std::cout << "Digite a data da ultima menstruação somente numeros:\n";
     //std::cin.ignore();
     std::getline(std::cin, dum);
     std::cout <<  dum.length()<<" caracteres "<< "\n\n";
 
     for (i=0;i<2;i++){
-        if (i=1){ 
+        
             stringtempo=stringtempo+dum[i];
             dumdia=std::stoi(stringtempo);
-            stringtempo="";
-        }
-        if (i=3) {
+                    
+    }
+    stringtempo="";
+    for (i=2;i<4;i++){
+        
             stringtempo=stringtempo+dum[i];
             dummes=std::stoi(stringtempo);
-            stringtempo="";
-        }
-        if (i=7) {
+            
+        
+    }
+    stringtempo="";
+    for (i=4;i<dum.length();i++){
             stringtempo=stringtempo+dum[i];
             dumano=std::stoi(stringtempo);
-            stringtempo="";
+            
         }        
-    }
-    std::cout << dumdia <<"/"<<dummes<<"/<<dumano"<<'\n';
+    
+
+
+    std::cout << dumdia <<"/"<<dummes<<"/"<<dumano<<'\n';
 
     
     //std::cout << "dia da dum= "<< dumdia << '\n';
     
 
     // preenche as datas
-    data1.tm_year = 2023 - 1900; // ano - 1900
-    data1.tm_mon = 11;            // mês (0-11)
-    data1.tm_mday = 12;           // dia
+    data1.tm_year = dumano - 1900; // ano - 1900
+    data1.tm_mon = dummes;            // mês (0-11)
+    data1.tm_mday = dumdia;           // dia
     data2.tm_year = 2024 - 1900;
     data2.tm_mon = 1;
     data2.tm_mday = 5;
+
 
     // converte as datas para segundos
     time_t segundos1 = mktime(&data1);
