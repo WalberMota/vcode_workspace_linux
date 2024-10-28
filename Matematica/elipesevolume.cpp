b1ef6984f4026a1ea17fd3f75f5a1774caddec94
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-10-28 20:35:46 -0300

    update elipsevolume.cpp

--- Matematica/elipesevolume.cpp
+++ Matematica/elipesevolume.cpp
@@ -1,25 +1,21 @@
-#include<iostream>
-#include<string>
-
+#include <iostream>
+#include <string>
 
 int main(int argc, char const *argv[])
 {
-    std::string r1,r2,r3;
+    std::string r1, r2, r3;
 
+    //std::cin.ignore();
     std::cout << "Digite o primeiro raio: ";
-    getline(std::cin,r1);
-    
-    std::cin.ignore();
+    getline(std::cin, r1);
+
     std::cout << "Digite o segundo raio: ";
-    getline(std::cin,r2);
-    
-    std::cin.ignore();
+    getline(std::cin, r2);
+
     std::cout << "Digite o terceiro raio: ";
-    getline(std::cin,r3);
+    getline(std::cin, r3);
 
-
-    std::cout <<"r1="<< r1<<" r2="<<r2<<" r3="<<r3<<std::endl;
+    std::cout << "r1=" << r1 << " r2=" << r2 << " r3=" << r3 << std::endl;
 
     return 0;
 }
-
