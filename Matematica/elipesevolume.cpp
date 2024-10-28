273723f98199e686b94b9da262d2eb1bbaf1e0fe
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-10-28 20:46:39 -0300

    update elipsevolume.cpp

--- Matematica/elipesevolume.cpp
+++ Matematica/elipesevolume.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <string>
 
+float calculavolume();
+
+    std::string r1, r2, r3;
+    float raio1,raio2,raio3;
+
 int main(int argc, char const *argv[])
 {
-    std::string r1, r2, r3;
+
 
     //std::cin.ignore();
     std::cout << "Digite o primeiro raio: ";
@@ -17,5 +22,19 @@ int main(int argc, char const *argv[])
 
     std::cout << "r1=" << r1 << " r2=" << r2 << " r3=" << r3 << std::endl;
 
+    float raio1=stoi(r1);
+    float raio2=stoi(r2);
+    float raio3=stoi(r3);
+    std::cout<< "Volume da elipse:"<<calculavolume();
+    
     return 0;
+}
+
+float calculavolume()
+{
+    float volume;
+
+    volume=(4*3.14*raio1*raio2*raio3)/4;
+
+    
 }
