9f3b69e09832f34dd0b321ca36488a2bf341e29c
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-02-16 19:41:10 -0300

    update arrays_omitindo tamanho

--- Estudos - Daniel Gakwaya/arrays/arrays_omitindoTamanho.cpp
+++ Estudos - Daniel Gakwaya/arrays/arrays_omitindoTamanho.cpp
@@ -3,8 +3,13 @@
 int main(int argc, char const *argv[])
 {
     int turma[]{10,12,15,11,18,17};
+/*
+Para que a matriz do array não seja modificada pode usar:
+const int turma[]{10,12,15,11,18,17}
+*/
 
-    for(auto valor:turma){
+//ler os valores do array
+for(auto valor:turma){
         std::cout <<"valor: "<<valor<<std::endl;
     }
     return 0;
