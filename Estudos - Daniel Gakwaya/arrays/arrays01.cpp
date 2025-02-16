117227396598fea8c4c29a4e15b284b8c162484a
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-02-16 19:14:32 -0300

    update arrays salarios - DGakwaya

--- Estudos - Daniel Gakwaya/arrays/arrays01.cpp
+++ Estudos - Daniel Gakwaya/arrays/arrays01.cpp
@@ -3,9 +3,9 @@
 int main(int argc, char const *argv[])
 {
     int scores[10]; //array estocando 10 inteiros
-    scores[0]= 20;
-    scores[1]= 21;
-    scores[2]= 22;
+    //scores[0]= 20;
+    //scores[1]= 21;
+    //scores[2]= 22;
     // scores[22]=300; //gravando fora dos limites do array - causa o erro "warning: array index 22 is past the end of the array (that has type 'int[10]') [-Warray-bounds]"
 
     for (size_t i = 0; i < 10; i++)
---
+++ Estudos - Daniel Gakwaya/arrays/arrays_salarios.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    double salarios[5]{12.7,7.5,13.2,8.1,9.3};
+    
+    return 0;
+}
+
+
+
+
