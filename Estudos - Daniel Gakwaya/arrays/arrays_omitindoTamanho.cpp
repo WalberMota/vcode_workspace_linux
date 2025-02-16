dd623a0953f287aac05cfa3e8d6ef852bfe1d09d
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-02-16 19:48:09 -0300

    update arrays_omitindo tamanho

--- Estudos - Daniel Gakwaya/arrays/arrays_omitindoTamanho.cpp
+++ Estudos - Daniel Gakwaya/arrays/arrays_omitindoTamanho.cpp
@@ -1,16 +1,27 @@
-# include <iostream>
+#include <iostream>
 
 int main(int argc, char const *argv[])
 {
-    int turma[]{10,12,15,11,18,17};
-/*
-Para que a matriz do array não seja modificada pode usar:
-const int turma[]{10,12,15,11,18,17}
-*/
+    int turma[]{10, 12, 15, 11, 18, 17};
+    int sum;
+    
+    /*
+    Para que a matriz do array não seja modificada pode usar:
+    const int turma[]{10,12,15,11,18,17}
+    */
 
-//ler os valores do array
-for(auto valor:turma){
-        std::cout <<"valor: "<<valor<<std::endl;
+    // ler os valores do array
+    for (auto valor : turma)
+    {
+        std::cout << "valor: " << valor << std::endl;
     }
+
+    //soma os valores do arry e guarda em uma variável qualquer
+
+    for(int elemento:turma){
+        sum+=elemento;
+        std::cout << std::endl;
+        std::cout <<"Resultado da soma dos elementos da matriz : "<< sum<<std::endl;
+        }
     return 0;
 }
