ec00e85513129c66b7e7b9cadcaf764de7910f63
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-02-16 19:16:51 -0300

    update arrays salarios - DGakwaya

--- Estudos - Daniel Gakwaya/arrays/arrays_salarios.cpp
+++ Estudos - Daniel Gakwaya/arrays/arrays_salarios.cpp
@@ -3,7 +3,13 @@
 int main(int argc, char const *argv[])
 {
     double salarios[5]{12.7,7.5,13.2,8.1,9.3};
-    
+
+    //lendo o arrays
+    for(size_t i=0;i<5;i++)
+    {
+        std::cout <<"salario["<<i<<"]:" << salarios[i]<<std::end;
+    }
+
     return 0;
 }
 
