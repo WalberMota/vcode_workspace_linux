9cf7e06773b250636c3544290d88c8acb418d5e4
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-03-01 07:52:31 -0300

    update test.cpp

--- Estudos - Daniel Gakwaya/arrays/test.cpp
+++ Estudos - Daniel Gakwaya/arrays/test.cpp
@@ -1,34 +1,29 @@
 #include <iostream>
 
-
 int data[]{1, 2, 4, 5, 1, 8, 2, 3, 6, 1, 4, 2};
-int select[]{};
+int seleciona[12]{};
 int numero;
-
 
 int main(int argc, char const *argv[])
 {
-    select[0]=data[0];
+    seleciona[0] = data[0];
 
-    for (int i=0;i<12;i++)
+    for (int i = 0; i < 12; i++)
     {
-        if (select[0]==data[i+1])
+        if (seleciona[i] == data[i + 1])
         {
             continue;
         }
         else
         {
-            select[i+1]=data[i+1];
+            seleciona[i + 1] = data[i + 1];
         }
-            
-    }
     }
 
-
-    //ler select[]
+    // ler select[]
     for (size_t i{0}; i < 12; i++)
-    std::cout << select[i] << " ";
-
-    
+    {
+        std::cout << seleciona[i] << " ";
+    }
     return 0;
 }
