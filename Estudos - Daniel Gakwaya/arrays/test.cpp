f20ecc9d2a98c19510a315a3f57d61dbfa6a7c2a
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-03-01 07:44:14 -0300

    update test.cpp

--- Estudos - Daniel Gakwaya/arrays/test.cpp
+++ Estudos - Daniel Gakwaya/arrays/test.cpp
@@ -12,11 +12,19 @@ int main(int argc, char const *argv[])
 
     for (int i=0;i<12;i++)
     {
-        if (select[0]!=data[i+1])
+        if (select[0]==data[i+1])
+        {
+            continue;
+        }
+        else
         {
             select[i+1]=data[i+1];
         }
-    }        
+            
+    }
+    }
+
+
     //ler select[]
     for (size_t i{0}; i < 12; i++)
     std::cout << select[i] << " ";
