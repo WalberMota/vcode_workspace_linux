0f2ff938ebe469d27580ca28e586ec9c7d3df96d
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-03-01 21:08:05 -0300

    update test.cpp

---
+++ Estudos - Daniel Gakwaya/arrays/tamanhovetor.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int vetor[4]{2,3,4,5};
+int main()
+{
+    std::cout <<"Tamanho do vetor :"<<sizeof(vetor)/4;
+    return 0;
+}
--- Estudos - Daniel Gakwaya/arrays/test.cpp
+++ Estudos - Daniel Gakwaya/arrays/test.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
 
 int data[]{1, 2, 4, 5, 1, 8, 2, 3, 6, 1, 4, 2};
-int seleciona[12]{};
-int numero;
-int i{0};
+int seleciona[]{0};
+int tamanho;
+
 int main()
 {
     seleciona[0] = data[0];
+    tamanho=sizeof(seleciona)/4;
 
-    do
+    for(int x=0;x<sizeof(data)/4;x++)
     {
-        for (int x : data)
-        {
-            if (seleciona[i] == data[i])
-            {
-                i++;
-                break;
+        for (int i=0;i<tamanho;i++){
+            if (seleciona[i]==data[x]){
+            
             }
-            else
-            {
-                seleciona[i + 1] =data[x];
-                i++;
+            else{
+                seleciona[i+1]=data[x];
+                tamanho=sizeof(seleciona)/4;
             }
-        }
-    } while (i < 12);
 
-    // ler select[]
+        }
+
+    }
+    
     for (size_t i{0}; i < 13; i++)
     {
         std::cout << seleciona[i] << " ";
