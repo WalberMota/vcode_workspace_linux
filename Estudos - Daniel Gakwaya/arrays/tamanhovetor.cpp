29bc6aa973babd6f6a295b56b6907edac3a99cd1
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-04-13 19:40:09 -0300

    tamanho de vetor

--- Estudos - Daniel Gakwaya/arrays/tamanhovetor.cpp
+++ Estudos - Daniel Gakwaya/arrays/tamanhovetor.cpp
@@ -1,8 +1,27 @@
-#include<iostream>
+#include <iostream>
 
-int vetor[4]{2,3,4,5};
+int vetor[4]{2, 3, 4, 5};
+int mostravetor();
+
 int main()
 {
-    std::cout <<"Tamanho do vetor :"<<sizeof(vetor)/4;
+    mostravetor();
+    std::cout << "]" << std::endl;
+    std::cout << "Tamanho do vetor usando sizeof():" << sizeof(vetor) / 4 << std::endl;
+    std::cout << "Tamanho do vetor usando std::size():" << std::size(vetor) << std::endl;
+
+    return 0;
+}
+
+int mostravetor()
+{
+    std::cout << "vetor=[";
+    for (int i = 0; i < std::size(vetor); i++)
+    {
+        std::cout << vetor[i];
+        if (i < std::size(vetor) - 1)
+            std::cout << ",";
+    };
+
     return 0;
 }
