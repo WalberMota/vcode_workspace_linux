fb984b5f1e6b36872e862c2b2de6827b53cedba0
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-04-15 11:05:51 -0300

    array de caracter

--- Estudos - Daniel Gakwaya/arrays/array_caracter.cpp
+++ Estudos - Daniel Gakwaya/arrays/array_caracter.cpp
@@ -3,14 +3,13 @@
 int main()
 {
 
-    // Declare array
+    // Declara o array
     char message[]{"Hello Dev"}; // o compilador automaticamente coloca \0 no fim do array
-    // int data[5] {1,2,3,3,3};
 
-    // std::cout << "data : " << data << std::endl;
+    int data[5]{1, 2, 3, 3, 3};
+    std::cout << "data : " << data << std::endl; // a saida será: "data : 0x7fff79c1f6e0"
 
-    // Print out the array through looping
-
+    // Imprimindo a matriz através de looping
     std::cout << "message : ";
     for (auto c : message)
     {
@@ -18,53 +17,41 @@ int main()
     }
     std::cout << std::endl;
 
- 
-     //Change characters in our array
-     message[1] = 'a';
+    // Modificando um caracter em nosso array
+    message[1] = 'a';
 
-     //Print out the array through looping
-     std::cout << "message : ";
-     for( auto c : message){
-         std::cout << c ;
-     }
-     std::cout << std::endl;
- 
+    // Imprima a matriz através de looping
+    std::cout << "message : ";
+    for (auto c : message)
+    {
+        std::cout << c;
+    }
+    std::cout << std::endl;
 
-    // Will probably print garbage after your char array
-    /*
+    // Provavelmente imprimirá lixo após sua matriz de caracteres
     std::cout << "message : " << message << std::endl;
-    */
 
-    // If a character array is null terminated, it's called as C-String
-    /*
-    char message1 [] {'H','e','l','l','o','\0'};
-    std::cout << "message1 : " << message1 << std::endl;
+    // Se um array de caracteres é encerrado com nulo, ele é chamado de C-String
+    char message1[]{'B', 'a', 'r', 'c', 'o', '\0'};
+    std::cout << "\nmessage1 : " << message1 << std::endl;
     std::cout << "sizeof(message1) : " << sizeof(message1) << std::endl;
-    */
 
-    /*
-     char message2 [6] {'H','e','l','l','o'};
-     std::cout << "message2 : " << message2 << std::endl;
-     std::cout << "sizeof(message2) : " << sizeof(message2) << std::endl;
+    char message2[6]{'P', 'a', 'p', 'e', 'l'};
+    std::cout << "\nmessage2 : " << message2 << std::endl;
+    std::cout << "sizeof(message2) : " << sizeof(message2) << std::endl;
 
-     char message3 [] {'H','e','l','l','o'}; // This is not a c string ,
-                                             //as there is not null character
-     std::cout << "message3 : " << message3 << std::endl;
-     std::cout << "sizeof(message3) : " << sizeof(message3) << std::endl;
-     */
+    char message3[]{'W', 'a', 'l', 'b','e','r'}; // Não é uma C-String, como não há caráter nulo
+    std::cout << "\nmessage3 : " << message3 << std::endl;
+    std::cout << "sizeof(message3) : " << sizeof(message3) << std::endl;
 
     // String literal
-    /*
-    char message4 [] {"Hello"};
-    std::cout << "message4 : " << message4 << std::endl;
+    char message4[]{"Hello"};
+    std::cout << "\nmessage4 : " << message4 << std::endl;
     std::cout << "sizeof(message4) : " << sizeof(message4) << std::endl;
-    */
 
-    // Can't safely print out arrays other than those of characters
-
-    /*     int numbers [] {1,2,3,4,5};
-        std::cout << "numbers :  " << numbers << std::endl;
-      */
+    // Não é possível imprimir com segurança matrizes diferentes das de caracteres
+    int numbers[]{1, 2, 3, 4, 5};
+    std::cout << "\nnumbers :  " << numbers << std::endl;
 
     return 0;
 }
