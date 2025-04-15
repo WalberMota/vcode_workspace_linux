350f76b412aae0fd06ff8a90a543022e66e08043
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-04-15 11:37:45 -0300

    verifica ordem de array

--- Estudos - Daniel Gakwaya/arrays/verifica_ordem.cpp
+++ Estudos - Daniel Gakwaya/arrays/verifica_ordem.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
 
-int data[]{1, 2, 4, 5, 8, 12, 13, 16, 71, 92};
+int numbers[]{1, 2, 4, 5, 8, 12, 13, 16, 71, 92};
 
-int collection_size{std::size(data)};
+int collection_size{std::size(numbers)};
 bool sorted;
 int main(int argc, char const *argv[])
 {
+    //imprime o array
+    std::cout <<"numbers[";
+    for(auto i:numbers){
+        std::cout <<i<<" ";
+    }
+    std::cout <<"]"<<std::endl;
+
+    //função de verificação de ordem crescente
     for (int i = 0; i < collection_size; i++)
     {
-        if (data[i] < data[1 + 1])
-            sorted = true;
+        if (numbers[i] < numbers[1 + 1])
+            sorted = true; //em ordem crescente
         else
-            sorted = false;
+            sorted = false; //fora de ordem crescente
             break;
     }
 
     if (sorted==true)
-        std::cout <<"array is sorted in incrementing order.";
+        std::cout <<"array is sorted in incrementing order."<<std::endl;
         else
-        std::cout <<"array is not sorted in incrementing order.";
+        std::cout <<"array is not sorted in incrementing order."<<std::endl;
     return 0;
 }
