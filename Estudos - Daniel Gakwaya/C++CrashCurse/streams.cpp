2a2fdde07931ca8c71a0b0fb5e6135d70bfcd0cd
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-04-19 19:22:12 -0300

    c++streams

--- Estudos - Daniel Gakwaya/C++CrashCurse/streams.cpp
+++ Estudos - Daniel Gakwaya/C++CrashCurse/streams.cpp
@@ -2,12 +2,19 @@
 
 int main(int argc, char const *argv[])
 {
+    std::string nome;
+    std::cout <<"Digite um nome:";
+    std::cin >> nome;
+
+
     std::cerr << "atenção!!\n";
     std::clog << "Esta é uma mensagem de log" << std::endl;
 
     // Usar std::flush garante que a saída seja exibida imediatamente,
     // o que pode ser crucial para aplicativos em tempo real ou quando
     // você precisa de feedback imediato.
+    // para garantir que cada atualização seja exibida imediatamente em
+    // vez de esperar que o buffer fique cheio.
     // Alternativamente, std::endl pode ser usado, ele insere
     // um caractere de nova linha e também limpa o fluxo
 
