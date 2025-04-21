6a064fd1d0a133b6453c7019969828ed7f4f005e
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-04-21 14:00:09 -0300

    randomizacao.cpp

--- Estudos - Daniel Gakwaya/C++CrashCurse/randomizacao.cpp
+++ Estudos - Daniel Gakwaya/C++CrashCurse/randomizacao.cpp
@@ -8,7 +8,7 @@ int main()
 {
     int result=std::rand();
     std::cout << "número: "<<result<<std::endl;
-    std::cout << "RAND_MAX"<< RAND_MAX<<std::endl;
+    std::cout << "RAND_MAX = "<< RAND_MAX<<std::endl;
 
     return 0;
 }
