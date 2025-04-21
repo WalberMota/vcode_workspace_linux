99950f967cfe8aa312c96288d71592153743d4c8
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-04-21 14:07:04 -0300

    randomizacao.cpp

--- Estudos - Daniel Gakwaya/C++CrashCurse/randomizacao.cpp
+++ Estudos - Daniel Gakwaya/C++CrashCurse/randomizacao.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 #include<cstdlib> /*srand, rand*/
-
+#include<ctime>
 
 
 
 int main()
 {
+    std::srand(std::time(nullptr));
+    std::cout <<std::time(nullptr)<<std::endl;
     int result=std::rand();
     std::cout << "número: "<<result<<std::endl;
     std::cout << "RAND_MAX = "<< RAND_MAX<<std::endl;
