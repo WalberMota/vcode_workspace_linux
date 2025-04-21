3abe8298657ef394d0841ffdcae447caccf42f90
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-04-21 14:11:05 -0300

    randomizacao.cpp

--- Estudos - Daniel Gakwaya/C++CrashCurse/randomizacao.cpp
+++ Estudos - Daniel Gakwaya/C++CrashCurse/randomizacao.cpp
@@ -6,8 +6,9 @@
 
 int main()
 {
-    std::srand(std::time(nullptr));
-    std::cout <<std::time(nullptr)<<std::endl;
+    // std::srand(std::time(nullptr)); => use o tempo atual como semente para gerador aleatório
+    std::srand(std::time({}));
+    //std::cout <<std::time(nullptr)<<std::endl;
     int result=std::rand();
     std::cout << "número: "<<result<<std::endl;
     std::cout << "RAND_MAX = "<< RAND_MAX<<std::endl;
