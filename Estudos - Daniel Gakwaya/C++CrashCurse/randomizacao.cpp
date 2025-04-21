852017d80388bd295b7eaa115da536f9b6017924
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-04-21 14:22:15 -0300

    randomizacao.cpp

--- Estudos - Daniel Gakwaya/C++CrashCurse/randomizacao.cpp
+++ Estudos - Daniel Gakwaya/C++CrashCurse/randomizacao.cpp
@@ -1,17 +1,18 @@
-#include<iostream>
-#include<cstdlib> /*srand, rand*/
-#include<ctime>
-
-
+#include <iostream>
+#include <cstdlib> /*srand, rand*/
+#include <ctime>
 
 int main()
 {
     // std::srand(std::time(nullptr)); => use o tempo atual como semente para gerador aleatório
     std::srand(std::time({}));
-    //std::cout <<std::time(nullptr)<<std::endl;
-    int result=std::rand();
-    std::cout << "número: "<<result<<std::endl;
-    std::cout << "RAND_MAX = "<< RAND_MAX<<std::endl;
+    std::cout << std::time({}) << std::endl;
+    int result = std::rand();
+    std::cout << "número: " << result << std::endl;
+    std::cout << "RAND_MAX = " << RAND_MAX << std::endl;
 
+    // numero gerado esteja dentro de uma faixa de variação.
+    int randon_number = randon_number % 10; // entre 1 e 10
+    std::cout << "entre 1 e 10" << randon_number << std::endl;
     return 0;
 }
