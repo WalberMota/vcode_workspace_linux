c05ce385d8e7c461c0fa167a0304164ff72a7684
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-04-25 17:35:01 -0300

    randomizacao.cpp

--- Estudos - Daniel Gakwaya/C++CrashCurse/randomizacao.cpp
+++ Estudos - Daniel Gakwaya/C++CrashCurse/randomizacao.cpp
@@ -6,14 +6,14 @@ int main()
 {
     // std::srand(std::time(nullptr)); => use o tempo atual como semente para gerador aleatório
     std::srand(std::time({}));
-    std::cout << std::time({}) << std::endl;
+    //std::cout << std::time({}) << std::endl;
     int result = std::rand();
-    std::cout << "número: " << result << std::endl;
-    std::cout << "RAND_MAX = " << RAND_MAX << std::endl;
+    //std::cout << "número: " << result << std::endl;
+    //std::cout << "RAND_MAX = " << RAND_MAX << std::endl;
 
     // numero gerado esteja dentro de uma faixa de variação.
     int randon_number = result % 10; // entre 0 e 10
-    std::cout << "Número entre 0 e 10: " << randon_number << std::endl;
+    //std::cout << "Número entre 0 e 10: " << randon_number << std::endl;
 
     //Pede ao usuário para adivinhar o número
 
