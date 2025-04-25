921741aa9b171dda88fe25d2c7b2045da6c3b819
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-04-25 17:53:49 -0300

    class constructors

--- Estudos - Daniel Gakwaya/C++CrashCurse/Classes/classconstructors.cpp
+++ Estudos - Daniel Gakwaya/C++CrashCurse/Classes/classconstructors.cpp
@@ -4,8 +4,15 @@ class Retangulo
 {
 public:
     // construtor
-    Retangulo(){}; //construtor sem parametros
-    Retangulo(int w, int l);//construtor com dois parametros
+    Retangulo() //construtor sem parametros
+    {
+        std::cout <<"camada de construtor de classe sem parâmetros"<<std::endl;
+    }
+    Retangulo(int w, int l)//construtor com dois parametros
+    {
+        m_width=w;
+        m_length=l;
+    }
 
     void SetWidth(int width);
     void SetLength(int length);
