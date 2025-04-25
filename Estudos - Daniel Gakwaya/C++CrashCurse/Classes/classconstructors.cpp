0d409a30db95e86cfa6467696fd3786f030ee46d
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-04-25 17:57:12 -0300

    class constructors

--- Estudos - Daniel Gakwaya/C++CrashCurse/Classes/classconstructors.cpp
+++ Estudos - Daniel Gakwaya/C++CrashCurse/Classes/classconstructors.cpp
@@ -4,22 +4,24 @@ class Retangulo
 {
 public:
     // construtor
-    Retangulo() //construtor sem parâmetros
+    Retangulo() // construtor sem parâmetros
     {
-        std::cout <<"camada de construtor de classe sem parâmetros"<<std::endl;
+        std::cout << "camada de construtor de classe sem parâmetros" << std::endl;
     }
-    Retangulo(int w, int l)//construtor com dois parâmetros
+    Retangulo(int w, int l) // construtor com dois parâmetros
     {
-        std::cout <<"chamada de construtor de classe com dois parâmetros"<<std::endl;
-        m_width=w;
-        m_length=l;
+        std::cout << "chamada de construtor de classe com dois parâmetros" << std::endl;
+        m_width = w;
+        m_length = l;
     }
 
-    void SetWidth(int width){
-        m_width=width;
+    void SetWidth(int width)
+    {
+        m_width = width;
     }
-    void SetLength(int length){
-        m_length=length;
+    void SetLength(int length)
+    {
+        m_length = length;
     }
 
     int getArea()
