f1ac2bc2f5b896f78f3d0b068521e1db71e642ff
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-04-25 17:43:07 -0300

    class constructors

--- Estudos - Daniel Gakwaya/C++CrashCurse/Classes/classconstructors.cpp
+++ Estudos - Daniel Gakwaya/C++CrashCurse/Classes/classconstructors.cpp
@@ -1,7 +1,28 @@
-#include<iostream>
+#include <iostream>
 
-int main()
+class Retangulo
 {
-    
+public:
+    // construtor
+    Retangulo();
+    Retangulo(int w, int l);
+
+    void SetWidth(int width);
+    void SetLength(int length);
+
+    int getArea()
+    {
+        return width * length;
+    }
+
+private:
+    int width;
+    int length;
+
+};
+
+main()
+{
+
     return 0;
 }
