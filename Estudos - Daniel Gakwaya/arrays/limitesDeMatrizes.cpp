aab3da34747777b80b656dc58c9043ebc1b18f36
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-05-11 15:00:35 -0300

    limitesDeMatrizes.cpp

--- Estudos - Daniel Gakwaya/arrays/limitesDeMatrizes.cpp
+++ Estudos - Daniel Gakwaya/arrays/limitesDeMatrizes.cpp
@@ -8,5 +8,14 @@ int main()
     std::cout << "\nTamanho do char(numeros1): "<<sizeof(numeros1)<<" elementos.";
     std::cout << "\nTamanho do int (numeros2): "<<sizeof(numeros2)<<" bytes."<<std::endl;
 
+    std::cout<<"char numero1= " ;
+    for(auto num:numeros1){
+        std::cout << toascii(num)<<",";
+    }
+    
+    std::cout << "\nEndereço de int numero2: "<< numeros2<< std::endl;
+
+    //std::cout <<numeros2[2000]<<std::endl;//gera segmentation fault
+
     return 0;
 }
