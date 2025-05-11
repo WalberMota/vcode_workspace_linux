771d7b532a6a75e5fb67778dd9ba7562fc93b750
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-05-11 14:41:09 -0300

    limitesDeMatrizes.cpp

--- Estudos - Daniel Gakwaya/arrays/limitesDeMatrizes.cpp
+++ Estudos - Daniel Gakwaya/arrays/limitesDeMatrizes.cpp
@@ -5,8 +5,8 @@ int numeros2[]{1,2,3,4};
 
 int main()
 {
-    std::cout << "Tamanho do char(numeros1): "<<sizeof(numeros1)<<" elementos.";
-    std::cout << "Tamanho do int (numeros2): "<<sizeof(numeros2)<<" bytes.";
+    std::cout << "\nTamanho do char(numeros1): "<<sizeof(numeros1)<<" elementos.";
+    std::cout << "\nTamanho do int (numeros2): "<<sizeof(numeros2)<<" bytes."<<std::endl;
 
     return 0;
 }
