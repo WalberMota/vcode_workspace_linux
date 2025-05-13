aebbfec8a3da7e2d654d34ee2495d0e92f3fa5b8
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-05-13 19:48:40 -0300

    numerosAleatorios.cpp

---
+++ Estudos - Daniel Gakwaya/arrays/numerosAleatorios.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+    int num_aleatorio = std::rand();
+    std::cout << "numero aleatório = "<<num_aleatorio<<std::endl; 
+    return 0;
+}
