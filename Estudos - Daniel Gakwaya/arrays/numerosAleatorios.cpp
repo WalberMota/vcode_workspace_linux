59c40c48561f1b7eaa88d816aa3da8136c68b5bb
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-05-13 20:06:12 -0300

    numerosAleatorios.cpp

--- Estudos - Daniel Gakwaya/arrays/numerosAleatorios.cpp
+++ Estudos - Daniel Gakwaya/arrays/numerosAleatorios.cpp
@@ -1,14 +1,23 @@
-#include<iostream>
+#include <iostream>
 
 int main(int argc, char const *argv[])
 {
     int num_aleatorio = std::rand();
-    std::cout << "numero aleatório = "<<num_aleatorio<<std::endl; 
+    std::cout << "numero aleatório = " << num_aleatorio << std::endl;
 
-    num_aleatorio=std::rand();
-    std::cout << "numero aleatório = "<<num_aleatorio<<std::endl; 
+    num_aleatorio = std::rand();
+    std::cout << "numero aleatório = " << num_aleatorio << std::endl;
 
     std::cout << "RAND_MAX = " << RAND_MAX << std::endl;
+
+    num_aleatorio = std::rand() % 11;
+    std::cout << "numero aleatório = " << num_aleatorio << std::endl;
+
+    for (size_t i{}; i < 10; i++)
+    {
+        num_aleatorio = std::rand() % 11;
+        std::cout << "numero aleatório = " << num_aleatorio << std::endl;
+    }
 
     return 0;
 }
