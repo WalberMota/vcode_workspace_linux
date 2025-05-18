647c129fccfd7d2c5ea24a5a795b03412d1b477f
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-05-18 09:37:11 -0300

    format.cpp

--- Operacoes de Conversao/format.cpp
+++ Operacoes de Conversao/format.cpp
@@ -1,11 +1,14 @@
 #include<format>
 #include<iostream>
-
+void println(std::format_string<Args...>fmt,Args&&...args)
+{
+    std::cout << std::format(fmt,std::format<Args>(args));
+}
 
 int main(int argc, char const *argv[])
 {
     auto valor=45;
-    //std::cout << std::format(fmt,std::format<Args>(args));
+    //
     println("{0:d} {0:#b}",valor);
 
 
