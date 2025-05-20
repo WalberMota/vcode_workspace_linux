f55ec54228ca7d0464b5d553680655eed8dd1df5
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-05-19 22:52:54 -0300

    format.cpp

--- Operacoes de Conversao/format_01.cpp
+++ Operacoes de Conversao/format_01.cpp
@@ -1,21 +1,20 @@
 //C++ Program to implement
 //C++ 20 - std::format
-#include <format>
+//#include <format>
 #include <iostream>
+#include <string>
 
-using namespace std;
+//using namespace std;
 
 int main()
 {
     // Declare variables
-    int num = 42;
-    std::string name = "John";
+    //int num = 42;
+    std::string nome = "John";
 
     // Use std::format to format a string with placeholders
     // for variables
-    std::string formatted_str = std::format(
-        "My name is {} and my favorite number is {}", name,
-        num);
+    std::string formatted_str = "My name is {} and my favorite number is {}", nome;
 
     // Print formatted string to console
     std::cout << formatted_str << std::endl;
