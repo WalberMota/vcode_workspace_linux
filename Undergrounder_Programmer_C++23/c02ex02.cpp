62cb47219e41b466ddcae5187ec9add3858c720d
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-06-17 22:29:34 -0300

    c02ex02.cpp - underground _programmer

---
+++ Undergrounder_Programmer_C++23/c02ex02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip> //defnidr a precisão das casas decimais
+
+int main(void)
+{
+    // define dois calores numericos do tipo float
+    float num1{10.5f};
+    float num2{3.2f};
+
+    // operações aritméticas
+    float soma = num1 + num2;
+    float subtracao = num1 - num2;
+    float multiplicacao = num1 * num2;
+    int16_t divInteira = static_cast<int16_t>(num1/num2);
+    float divReal= num1/num2;
+    
+
+}
