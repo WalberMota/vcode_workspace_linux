a7c7a9d71d6bb6f7099c598653cd30e15619579c
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2025-06-28 13:32:11 -0300

    maisponteiro.cpp

--- Ponteiros/maisponteiro.cpp
+++ Ponteiros/maisponteiro.cpp
@@ -4,22 +4,27 @@ void soma(int x, int y, int z)
     z = x + y;
 }
 
+
 void soma2(int *x,int *y,int *z)
 {
     *z = *x + *y;
 }
 
+void soma3(int &x,int &y,int &z)
+{
+    z = x + y;
+}
 int main()
 {
 
     int a = 10, b = 20, c = 0;
 
     soma(a, b, c);
-    std::cout << "Valor de c (por valor): " << c << "\n";
 
 
     soma2(&a, &b, &c);
-    std::cout << "Valor de c (por valor): " << c << "\n";
+    std::cout << "Valor de c (por ref): " << c << "\n";
+    
     return 0;
 }
 
